32e899472c24c50a9ff0e7a5d6cf3483e84b3dba
Henning Jacobs <henning@jacobs1.de>
2015-03-20 16:27:18 +0100

    next attempt to get motd right

--- build/setup.d/89-motd.sh
+++ build/setup.d/89-motd.sh
@@ -1,4 +1,6 @@
+mv /etc/update-motd.d/00-taupage /temp-motd
 rm /etc/update-motd.d/*
+mv /temp-motd /etc/update-motd.d/00-taupage
 bash /etc/update-motd.d/00-taupage > /etc/motd
 rm /run/motd.dynamic
 rm /etc/legal
