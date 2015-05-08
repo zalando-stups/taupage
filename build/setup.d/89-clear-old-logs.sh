e5496f539905bad2ceee951ae7b764cf7b52d7c2
Henning Jacobs <henning@jacobs1.de>
2015-05-08 11:45:29 +0200

    fix start-berry script to return exit code zero if nothing was done

--- build/setup.d/89-clear-old-logs.sh
+++ build/setup.d/89-clear-old-logs.sh
@@ -1,5 +1,5 @@
 echo "Clearing old logs..."
 
-for log in /var/log/*.log /var/log/syslog; do
+for log in /var/log/*.log /var/log/syslog /var/log/upstart/*.log; do
     echo -n > $log
 done
--- runtime/opt/zalando/init.d/13-start-berry.sh
+++ runtime/opt/zalando/init.d/13-start-berry.sh
@@ -2,3 +2,5 @@
 
 # only start berry service if "mint_bucket" was defined
 grep 'mint_bucket' /etc/zalando.yaml && service berry start
+
+exit 0
