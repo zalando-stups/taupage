d5a39ec5713d808b84d85f7562d4e04e735934b4
Henning Jacobs <henning@jacobs1.de>
2015-05-12 17:40:30 +0200

    add example "secret" dir

---
+++ secret/secret-vars.sh
@@ -0,0 +1,8 @@
+# Example "secret-vars.sh"
+# this file contains the "private" information
+# baked into the AMI
+
+# even SSH access granting service URL
+EVEN_URL=https://even.stups.example.org
+LOGGLY_TOKEN=NOT_SET
+NEWRELIC_LICENSE_KEY=NOT_SET
