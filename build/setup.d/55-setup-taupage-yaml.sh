276e8557d17a4695d735ecb5cdceccc6bbf9699d
Henning Jacobs <henning@jacobs1.de>
2015-05-27 14:20:23 +0200

    bake instance_logs_url into AMI

--- build/setup.d/55-setup-taupage-yaml.sh
+++ build/setup.d/55-setup-taupage-yaml.sh
@@ -3,3 +3,6 @@
 if [ -n "$TOKEN_SERVICE_URL" ]; then
     echo "token_service_url: $TOKEN_SERVICE_URL" >> /etc/taupage.yaml
 fi
+if [ -n "$INSTANCE_LOGS_URL" ]; then
+    echo "instance_logs_url: $INSTANCE_LOGS_URL" >> /etc/taupage.yaml
+fi
