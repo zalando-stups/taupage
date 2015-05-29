c7b2c5eaa9c22ef8eea3c1de20f93a7168d3aa7b
Henning Jacobs <henning@jacobs1.de>
2015-05-29 11:03:12 +0200

    use health_check_path for etcd heartbeat

--- runtime/opt/zalando/bin/register-in-etcd.py
+++ runtime/opt/zalando/bin/register-in-etcd.py
@@ -9,6 +9,39 @@ import requests
 import socket
 
 
+def get_first(iterable, default=None):
+    if iterable:
+        for item in iterable:
+            return item
+    return default
+
+
+def get_health_check_url(config: dict):
+    health_check_port = config.get('health_check_port', get_first(sorted(config.get('ports', {}).keys())))
+    health_check_path = config.get('health_check_path')
+
+    if not health_check_path:
+        logging.info('Health check path is not configured, not checking application health')
+        return
+    if not health_check_port:
+        logging.warning('Health check port is not configured, skipping health check')
+        return
+
+    url = 'http://localhost:{}{}'.format(health_check_port, health_check_path)
+    return url
+
+
+def is_healthy(url):
+    '''Return true iff GET on the URL returns status 200'''
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            return True
+    except:
+        return False
+    return False
+
+
 def run_heartbeat(args):
     with open(args.config) as fd:
         config = yaml.load(fd)
@@ -22,19 +55,21 @@ def run_heartbeat(args):
     logging.info("Sending heartbeat for me ({}) to etcd cluster {} every {} seconds with {} seconds tolerance..".format(
                  hostname, args.etcd, args.interval, args.ttl))
 
+    health_check_url = get_health_check_url(config)
+
     while True:
-        # TODO support heartbeat checks to own application and only send to etcd if alive
+        # support heartbeat checks to own application and only send to etcd if alive
+        if not health_check_url or is_healthy(health_check_url):
+            # push metadata as /taupage/$hostname->$metadata
+            key = "taupage/{}".format(hostname)
+            value = metadata
 
-        # push metadata as /taupage/$hostname->$metadata
-        key = "taupage/{}".format(hostname)
-        value = metadata
-
-        data = {'value': value, 'ttl': args.ttl}
-        response = requests.put("{}/v2/keys/{}".format(args.etcd, key), data=data, stream=False)
-        if response.status_code < 200 or response.status_code >= 300:
-            logging.warn("Could not send heartbeat to etcd cluster {}: {}".format(args.etcd, response.status_code))
-        elif args.logging:
-            logging.info("Heartbeat: {} ttl={}".format(key, args.ttl))
+            data = {'value': value, 'ttl': args.ttl}
+            response = requests.put("{}/v2/keys/{}".format(args.etcd, key), data=data, stream=False)
+            if response.status_code < 200 or response.status_code >= 300:
+                logging.warn("Could not send heartbeat to etcd cluster {}: {}".format(args.etcd, response.status_code))
+            elif args.logging:
+                logging.info("Heartbeat: {} ttl={}".format(key, args.ttl))
 
         time.sleep(args.interval)
 
