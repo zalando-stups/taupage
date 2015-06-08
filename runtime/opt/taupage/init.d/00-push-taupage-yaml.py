4cd66ffacff4afca01341e6f84ce325c0d35122a
Henning Jacobs <henning@jacobs1.de>
2015-06-08 18:17:08 +0200

    use OAuth credentials to push Taupage YAML to fullstop. (#50)

--- runtime/opt/taupage/init.d/00-push-taupage-yaml.py
+++ runtime/opt/taupage/init.d/00-push-taupage-yaml.py
@@ -4,8 +4,13 @@ import boto.utils
 import codecs
 import json
 import logging
+import os
 import requests
+import time
 import yaml
+import zign.api
+
+CREDENTIALS_DIR = '/meta/credentials'
 
 
 def is_sensitive_key(k):
@@ -40,6 +45,33 @@ def mask_dictionary(d: dict):
     return masked_dict
 
 
+def get_token(config: dict):
+    token_url = config.get('token_service_url')
+
+    if not token_url:
+        logging.warning('No token service URL configured in Taupage YAML ("token_service_url" property)')
+        return
+
+    path = os.path.join(CREDENTIALS_DIR, 'user.json')
+
+    while not os.path.exists(path):
+        logging.info('Waiting for berry to download OAuth credentials to {}..'.format(path))
+        time.sleep(5)
+
+    with open(path) as fd:
+        credentials = json.load(fd)
+
+    user = credentials.get('application_username')
+    passwd = credentials.get('application_password')
+
+    if not user or not passwd:
+        logging.warning('Invalid OAuth credentials: application user and/or password missing in %s', path)
+        return
+
+    token = zign.api.get_named_token('uid', 'services', 'taupage', user, passwd, url=token_url, use_keyring=False)
+    return token
+
+
 def main():
     with open('/etc/taupage.yaml') as fd:
         config = yaml.safe_load(fd)
@@ -49,6 +81,8 @@ def main():
     if instance_logs_url:
         logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
         logging.getLogger('urllib3.connectionpool').setLevel(logging.WARN)
+
+        token = get_token() or {}
 
         # identity = {'region': 'eu-west-1', 'accountId': 123456, 'instanceId': 'i-123'}
         identity = boto.utils.get_instance_identity()['document']
@@ -76,8 +110,9 @@ def main():
         logging.info('Pushing Taupage YAML to {}..'.format(instance_logs_url))
         try:
             # TODO: use OAuth credentials
-            response = requests.post(instance_logs_url, data=json.dumps(data),
-                                     headers={'Content-Type': 'application/json'}, timeout=5)
+            response = requests.post(instance_logs_url, data=json.dumps(data), timeout=5,
+                                     headers={'Content-Type': 'application/json',
+                                              'Authorization': 'Bearer {}'.format(token.get('access_token'))})
             if response.status_code != 201:
                 logging.warn('Failed to push Taupage YAML: server returned HTTP status {}: {}'.format(
                              response.status_code, response.text))
