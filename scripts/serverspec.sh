073657bf50fda5b3d3b7adae87a145951183d13b
Felix Mueller <felix.mueller@zalando.de>
2015-06-10 15:47:46 +0200

    Fixed serverspec

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Preparing Serverspec and running system tests..."
 # sudo apt-get update &&
-sudo apt-get install -y rubygems1.9.1 ruby-dev
-sudo gem install bundler --no-ri --no-rdoc
+apt-get install -y ruby1.9.1
+gem install bundler --no-ri --no-rdoc
+chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
 bundle install --path=vendor
 bundle exec rake spec
