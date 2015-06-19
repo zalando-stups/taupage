298cc8941bc5cb7c7c9cf2d94c19feb1cd79556a
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-06-19 18:22:59 +0200

    correct comments

--- runtime/opt/taupage/init.d/91-setup-rkhunter.py
+++ runtime/opt/taupage/init.d/91-setup-rkhunter.py
@@ -2,8 +2,8 @@
 from subprocess import call
 
 
-#update rkhunter database
+# update rkhunter database
 call(["rkhunter", "--propupd"])
 
-#initial rkhunter check and write logs to /var/log/rkhunter.log
+# initial rkhunter check and write logs to /var/log/rkhunter.log
 call(["rkhunter", "-c", "--sk", "--rwo"])
