e6aa0aaab4fd7b4b92b87a8c6cf206826bb621be
Henning Jacobs <henning@jacobs1.de>
2015-06-23 16:32:48 +0200

    #84 add test to verify that the "aufs" storage driver is used

--- tests/spec/docker_spec.rb
+++ tests/spec/docker_spec.rb
@@ -8,3 +8,8 @@ describe service('docker') do
   it { should be_enabled   }
   it { should be_running   }
 end
+
+describe command('docker info') do
+  # make sure the aufs module can be loaded and is used by Docker
+  its(:stdout) { should match /Storage Driver: aufs/ }
+end
