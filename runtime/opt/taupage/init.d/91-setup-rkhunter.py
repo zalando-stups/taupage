5b6de2dedbcf7d9e8f0bd0dd352b3930bc7aaaf3
Henning Jacobs <henning@jacobs1.de>
2015-06-23 18:37:02 +0200

    add TODO

--- runtime/opt/taupage/init.d/91-setup-rkhunter.py
+++ runtime/opt/taupage/init.d/91-setup-rkhunter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from subprocess import call
 
+# TODO: running rkhunter during boot takes about one minute,
+# that's far too slow! we should run it only in the background
 
 # update rkhunter database
 call(["rkhunter", "--propupd"])
