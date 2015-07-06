bec09f8fab61d104259a514412e707b2a73d6abf
Marco Hoyer <marco@hoyer.pm>
2015-07-06 20:45:02 +0200

    clean installation files after install

--- build/setup.d/71-cloudwatch-logs.sh
+++ build/setup.d/71-cloudwatch-logs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+mkdir /tmp/cw-logs-setup
+cd /tmp/cw-logs-setup
+
 # put empty config file as placeholder for the real config file
-touch /tmp/cloudwatch_logs_empty.conf
+touch cloudwatch_logs_empty.conf
 
 # download and install latest version of the cloudwatch logs agent
 wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
 chmod +x ./awslogs-agent-setup.py
 
 # the region parameter is not relevant here but required
-./awslogs-agent-setup.py -n -r foo -c
+./awslogs-agent-setup.py -n -r foo -c cloudwatch_logs_empty.conf
+
+# cleanup
+rm -rf /tmp/cw-logs-setup
