95fb5f120c95499495344ad5fcd4d68a701890d2
Marco Hoyer <marco@hoyer.pm>
2015-07-07 00:58:40 +0200

    add basic system test verifying that the config customization is done and the service is running

---
+++ tests/spec/awslogs_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe service('awslogs') do
+  it { should be_running }
+end
+
+describe file('/var/awslogs/etc/aws.conf') do
+  it { should contain 'cwlogs = cwlogs' }
+end
+
+describe file('/var/awslogs/etc/awslogs.conf') do
+  it { should contain 'state_file = /var/awslogs/state/agent-state' }
+  it { should contain '[/var/log/syslog]' }
+  it { should contain '[/var/log/application.log]' }
+end
