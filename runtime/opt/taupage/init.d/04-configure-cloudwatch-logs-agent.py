2b45d0cb4baad74ae6624a4b375029b7cc8527d1
Marco Hoyer <marco@hoyer.pm>
2015-07-06 23:21:00 +0200

    start awslogs service after configuring it and write config files to the correct destination

--- runtime/opt/taupage/init.d/04-configure-cloudwatch-logs-agent.py
+++ runtime/opt/taupage/init.d/04-configure-cloudwatch-logs-agent.py
@@ -4,6 +4,7 @@ import boto.utils
 import logging
 import sys
 import textwrap
+import subprocess
 
 from jinja2 import Template
 from taupage import configure_logging, get_config
@@ -43,6 +44,13 @@ def write_file(path, content):
         file.write(content)
 
 
+def start_awslogs_service():
+    process = subprocess.Popen(['service', 'awslogs', 'start'])
+    exit_code = process.wait()
+    if exit_code:
+        raise Exception("'service awslogs start' failed with exit code: {0}".format(exit_code))
+
+
 def main():
     configure_logging()
     config = get_config()
@@ -53,7 +61,7 @@ def main():
 
     logging.info('Configuring Cloudwatch Logs Agent')
 
-    # identity = {'region': 'eu-west-1', 'accountId': 123456, 'instanceId': 'i-123'}
+    #identity = {'region': 'eu-west-1', 'accountId': 123456, 'instanceId': 'i-123'}
     identity = boto.utils.get_instance_identity()['document']
 
     environment = {
@@ -66,8 +74,12 @@ def main():
     }
 
     try:
-        write_file('/tmp/aws.conf', render_template(AWS_CONFIG_TEMPLATE, environment))
-        write_file('/tmp/awslogs.conf', render_template(AWSLOGS_CONFIG_TEMPLATE, environment))
+        write_file('/var/awslogs/etc/aws.conf', render_template(AWS_CONFIG_TEMPLATE, environment))
+        write_file('/var/awslogs/etc/awslogs.conf', render_template(AWSLOGS_CONFIG_TEMPLATE, environment))
+
+        start_awslogs_service()
+
+        logging.info('Successfully configured and started Cloudwatch Logs Agent')
     except Exception as e:
         logging.error('Failed to configure Cloudwatch Logs Agent')
         logging.exception(e)
