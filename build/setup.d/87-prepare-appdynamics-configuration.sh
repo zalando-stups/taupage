583349ef9ab33c7a0077db989dfa085daf3c8d38
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-07-14 18:17:36 +0200

    modify sed commands

--- build/setup.d/87-prepare-appdynamics-configuration.sh
+++ build/setup.d/87-prepare-appdynamics-configuration.sh
@@ -30,18 +30,18 @@ for conf in $appdynamics_configs; do
 	echo "INFO: preparing AppDynamics config $conf"
 
 	# controller config
-	sed -i "1,$ s/controller-host.*$/<controller-host>$APPDYNAMICS_CONTROLLER_HOST<\/controller-host>/" $conf
-	sed -i "1,$ s/controller-port.*$/<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-port>/" $conf
-	sed -i "1,$ s/controller-ssl-enabled.*$/<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL<\/controller-ssl-enabled>/" $conf
+	sed -i "1,$ s/<controller-host.*$/<controller-host>$APPDYNAMICS_CONTROLLER_HOST<\/controller-host>/" $conf
+	sed -i "1,$ s/<controller-port.*$/<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-port>/" $conf
+	sed -i "1,$ s/<controller-ssl-enabled.*$/<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL<\/controller-ssl-enabled>/" $conf
 
 	# access config
-	sed -i "1,$ s/account-name.*$/<account-name>$APPDYNAMICS_ACCOUNT_NAME<\/account-name>/" $conf
-	sed -i "1,$ s/account-access-key.*$/<account-access-key>$APPDYNAMICS_ACCOUNT_ACCESS_KEY<\/account-access-key>/" $conf
+	sed -i "1,$ s/<account-name.*$/<account-name>$APPDYNAMICS_ACCOUNT_NAME<\/account-name>/" $conf
+	sed -i "1,$ s/<account-access-key.*$/<account-access-key>$APPDYNAMICS_ACCOUNT_ACCESS_KEY<\/account-access-key>/" $conf
 
 	# runtime config
-	sed -i "1,$ s/application-name.*$/<application-name>APPDYNAMICS_APPLICATION<\/application-name>/" $conf
-	sed -i "1,$ s/tier-name.*$/<tier-name>APPDYNAMICS_TIER<\/tier-name>/" $conf
-	sed -i "1,$ s/node-name.*$/<node-name>APPDYNAMICS_NODE<\/node-name>/" $conf
+	sed -i "1,$ s/<application-name.*$/<application-name>APPDYNAMICS_APPLICATION<\/application-name>/" $conf
+	sed -i "1,$ s/<tier-name.*$/<tier-name>APPDYNAMICS_TIER<\/tier-name>/" $conf
+	sed -i "1,$ s/<node-name.*$/<node-name>APPDYNAMICS_NODE<\/node-name>/" $conf
 
 	# register config for runtime
 	echo $conf >> /opt/proprietary/appdynamics-configs
