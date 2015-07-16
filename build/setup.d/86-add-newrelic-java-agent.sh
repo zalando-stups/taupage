4cac9ec02a78554e31b136ba216c47708521a1d6
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-07-16 14:22:18 +0200

    change the agent test's and don't stop the main setup.sh process

--- build/setup.d/86-add-newrelic-java-agent.sh
+++ build/setup.d/86-add-newrelic-java-agent.sh
@@ -7,26 +7,26 @@ newrelic_yaml=/opt/proprietary/newrelic/newrelic.yml
 #check if there is a no_newrelic file and exit
 if [ ! -d /opt/proprietary/newrelic ]; then
 	echo "INFO: no NewRelic agent found, skip configuration"
-	exit
+else
+
+	# standard config files
+	newrelic_agents="
+	/opt/proprietary/newrelic"
+
+	############################
+	# CHANGE DEFAULT YAML FILE #
+	############################
+
+	echo "INFO: change default newrelic.yaml"
+
+	#change newrelicToken to default
+	sed -i "1,$ s/license_key:.*$/license_key:\ LICENSEKEY/" $newrelic_yaml
+
+	#set high security
+	sed -i "1,$ s/high_security:.*$/high_security:\ true/" $newrelic_yaml
+
+	#change my application
+	sed -i "1,$ s/app_name:.*$/app_name:\ APPNAME/" $newrelic_yaml
+
+	#TODO maybe change more default settings
 fi
-
-# standard config files
-newrelic_agents="
-/opt/proprietary/newrelic"
-
-############################
-# CHANGE DEFAULT YAML FILE #
-############################
-
-echo "INFO: change default newrelic.yaml"
-
-#change newrelicToken to default
-sed -i "1,$ s/license_key:.*$/license_key:\ LICENSEKEY/" $newrelic_yaml
-
-#set high security
-sed -i "1,$ s/high_security:.*$/high_security:\ true/" $newrelic_yaml
-
-#change my application
-sed -i "1,$ s/app_name:.*$/app_name:\ APPNAME/" $newrelic_yaml
-
-#TODO maybe change more default settings
--- build/setup.d/87-prepare-appdynamics-configuration.sh
+++ build/setup.d/87-prepare-appdynamics-configuration.sh
@@ -3,54 +3,53 @@
 set -x
 
 # check if directory exists
-
 if [ ! -d /opt/proprietary/appdynamics-machine ]; then
 	echo "INFO: no AppDynamics agent found, skip configuration"
-	exit
-fi
+else
 
-# standard config files
-appdynamics_agents="
-/opt/proprietary/appdynamics-jvm
-/opt/proprietary/appdynamics-machine
-"
+	# standard config files
+	appdynamics_agents="
+	/opt/proprietary/appdynamics-jvm
+	/opt/proprietary/appdynamics-machine
+	"
 
-appdynamics_config=
-for agent in $appdynamics_agents; do
-	for conf in $(find $agent -name 'controller-info.xml'); do
-		appdynamics_configs="$appdynamics_configs $conf"
+	appdynamics_config=
+	for agent in $appdynamics_agents; do
+		for conf in $(find $agent -name 'controller-info.xml'); do
+			appdynamics_configs="$appdynamics_configs $conf"
+		done
 	done
-done
 
-# setup all configs
-for conf in $appdynamics_configs; do
-	if [ -z "$APPDYNAMICS_CONTROLLER_HOST" ] || [ -z "$APPDYNAMICS_ACCOUNT_KEY" ]; then
-		echo "ERROR: AppDynamics agent found but configuration missing; check your secrets configuration."
-		exit 1
-	fi
+	# setup all configs
+	for conf in $appdynamics_configs; do
+		if [ -z "$APPDYNAMICS_CONTROLLER_HOST" ] || [ -z "$APPDYNAMICS_ACCOUNT_KEY" ]; then
+			echo "ERROR: AppDynamics agent found but configuration missing; check your secrets configuration."
+			exit 1
+		fi
 
-	echo "INFO: preparing AppDynamics config $conf"
+		echo "INFO: preparing AppDynamics config $conf"
 
-	# controller config
-	sed -i "1,$ s/<controller-host.*$/<controller-host>$APPDYNAMICS_CONTROLLER_HOST<\/controller-host>/" $conf
-	sed -i "1,$ s/<controller-port.*$/<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-port>/" $conf
-	sed -i "1,$ s/<controller-ssl-enabled.*$/<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL<\/controller-ssl-enabled>/" $conf
+		# controller config
+		sed -i "1,$ s/<controller-host.*$/<controller-host>$APPDYNAMICS_CONTROLLER_HOST<\/controller-host>/" $conf
+		sed -i "1,$ s/<controller-port.*$/<controller-port>$APPDYNAMICS_CONTROLLER_PORT<\/controller-port>/" $conf
+		sed -i "1,$ s/<controller-ssl-enabled.*$/<controller-ssl-enabled>$APPDYNAMICS_CONTROLLER_SSL<\/controller-ssl-enabled>/" $conf
 
-	# access config
-	sed -i "1,$ s/<account-name.*$/<account-name>$APPDYNAMICS_ACCOUNT_NAME<\/account-name>/" $conf
-	sed -i "1,$ s/<account-access-key.*$/<account-access-key>$APPDYNAMICS_ACCOUNT_KEY<\/account-access-key>/" $conf
+		# access config
+		sed -i "1,$ s/<account-name.*$/<account-name>$APPDYNAMICS_ACCOUNT_NAME<\/account-name>/" $conf
+		sed -i "1,$ s/<account-access-key.*$/<account-access-key>$APPDYNAMICS_ACCOUNT_KEY<\/account-access-key>/" $conf
 
-	# runtime config
-	sed -i "1,$ s/<application-name.*$/<application-name>APPDYNAMICS_APPLICATION<\/application-name>/" $conf
-	sed -i "1,$ s/<tier-name.*$/<tier-name>APPDYNAMICS_TIER<\/tier-name>/" $conf
-	sed -i "1,$ s/<node-name.*$/<node-name>APPDYNAMICS_NODE<\/node-name>/" $conf
+		# runtime config
+		sed -i "1,$ s/<application-name.*$/<application-name>APPDYNAMICS_APPLICATION<\/application-name>/" $conf
+		sed -i "1,$ s/<tier-name.*$/<tier-name>APPDYNAMICS_TIER<\/tier-name>/" $conf
+		sed -i "1,$ s/<node-name.*$/<node-name>APPDYNAMICS_NODE<\/node-name>/" $conf
 
-	# register config for runtime
-	echo $conf >> /opt/proprietary/appdynamics-configs
-done
+		# register config for runtime
+		echo $conf >> /opt/proprietary/appdynamics-configs
+	done
 
-# make sure they are writeable by docker users
-for agent in $appdynamics_agents; do
-	chown -R root:root $agent
-	find $agent -type d -exec chmod 0777 '{}' ';'
-done
+	# make sure they are writeable by docker users
+	for agent in $appdynamics_agents; do
+		chown -R root:root $agent
+		find $agent -type d -exec chmod 0777 '{}' ';'
+	done
+fi
