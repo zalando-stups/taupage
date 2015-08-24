a1e721609393501328f7230af56821c6f9315c1f
Felix Mueller <felix.mueller@zalando.de>
2015-08-24 17:06:08 +0200

    This removes the AMI creator ssh key on boot and the ubuntu user. Fixes #4

---
+++ runtime/opt/taupage/init.d/92-remove-users.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$(getent passwd ubuntu)" ]; then
+    echo "User does not exists...skipping"
+else
+    echo "Deleting user ubuntu."
+    deluser ubuntu
+fi
+
+# Remove users authorized_keys on boot if exists
+if [ -f /root/.ssh/authorized_keys ]; then
+    echo "Deleting /root/.ssh/authorized_keys"
+    rm /root/.ssh/authorized_keys
+else
+    echo "/root/.ssh/authorized_keys does not exists...skipping"
+fi
