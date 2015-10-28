a2517d3715f24a970568c688e985adc6cec4a209
Felix Mueller <felix.mueller@zalando.de>
2015-10-28 10:44:31 +0100

    more verbosity

--- build/setup.d/10-upgrade-base.sh
+++ build/setup.d/10-upgrade-base.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -x
+
 echo "Updating system..."
 
 sudo rm -rf /var/lib/apt/lists/*
 apt-get update -y  # -q >>/tmp/build/upgrade.log
-apt-get dist-upgrade -y -q >>/tmp/build/upgrade.log
+apt-get dist-upgrade -y # -q  >>/tmp/build/upgrade.log
