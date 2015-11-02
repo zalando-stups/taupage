ade6efd1c352fe8417d1f9434e7d98e0e81b7f8a
Felix Mueller <felix.mueller@zalando.de>
2015-11-02 09:38:54 +0100

    modified granting tmpfs shell script

--- runtime/opt/taupage/init.d/00-create-ssh-granting-tmpfs.sh
+++ runtime/opt/taupage/init.d/00-create-ssh-granting-tmpfs.sh
@@ -2,16 +2,10 @@
 # This script is supposed to serve a backup solution if the filesystem
 # is full. It should provide the option for the user to ssh as root.
 
-# Create file system
-if [ ! -d /ssh-granting-service ]; then
-  mkdir -p /ssh-granting-service
-  mount -t tmpfs -o size=20m tmpfs /ssh-granting-service
-fi
-
-# Create directories
-if [ ! -d /ssh-granting-service/root/.ssh/ ]; then
-  mkdir -p /ssh-granting-service/root/.ssh/
-  touch /ssh-granting-service/root/.ssh/authorized_keys
-  chmod 600 /ssh-granting-service/root/.ssh/authorized_keys
-  ln -s /ssh-granting-service/root/.ssh/authorized_keys /root/.ssh/
+if [ ! -d /run/user/root/.ssh/ ]; then
+  mkdir -p /run/user/root/.ssh
+  chmod 700 /run/user/root/.ssh
+  touch /run/user/root/.ssh/authorized_keys
+  chmod 600 /run/user/root/.ssh/authorized_keys
+  ln -s /run/user/root/.ssh/authorized_keys /root/.ssh/
 fi
