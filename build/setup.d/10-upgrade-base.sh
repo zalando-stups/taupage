47f98f130dfc4d7395a3c3c0c1b5a2f6b03e2f3c
Felix Mueller <felix.mueller@zalando.de>
2015-11-23 13:50:13 +0100

    Fix dist-upgrade for now.

--- build/setup.d/10-upgrade-base.sh
+++ build/setup.d/10-upgrade-base.sh
@@ -6,4 +6,6 @@ echo "Updating system..."
 
 sudo rm -rf /var/lib/apt/lists/*
 apt-get update -y  # -q >>/tmp/build/upgrade.log
-apt-get dist-upgrade -y # -q  >>/tmp/build/upgrade.log
+apt-mark hold openssh-server
+apt-get dist-upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y # -q  >>/tmp/build/upgrade.log
+aptitude unhold openssh-server
