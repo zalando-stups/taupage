2249ed844296f3ed3b94d59a906189e89dc0c32b
Jens Zastrow <jens.zastrow_external@immobilienscout24.de>
2015-11-23 16:01:16 +0100

    fix

--- build/setup.d/05-add-repository-keys.sh
+++ build/setup.d/05-add-repository-keys.sh
@@ -25,5 +25,5 @@ echo 'deb http://rep.logentries.com/ trusty main' > /etc/apt/sources.list.d/loge
 gpg --keyserver pgp.mit.edu --recv-keys C43C79AD && gpg -a --export C43C79AD | apt-key add -
 
 # http://docs.docker.com/engine/installation/ubuntulinux/
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
