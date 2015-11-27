a5a50d9c865b41f45b54123b40de595537909e49
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-11-27 17:17:21 +0100

    remove deuplicated newrelic repo

--- build/setup.d/05-add-repository-keys.sh
+++ build/setup.d/05-add-repository-keys.sh
@@ -31,5 +31,5 @@ wget -qO- http://mirror.yandex.ru/mirrors/docker/DOCKER-GPG-KEY | apt-key add -
 
 
 # add Newrelic repo
-echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' >> /etc/apt/sources.list.d/newrelic.list
-wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
+# echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' >> /etc/apt/sources.list.d/newrelic.list
+# wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
