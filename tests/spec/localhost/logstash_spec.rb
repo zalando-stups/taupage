920f43d0a0b40f77ea2aeacfd17ed0a27cc6ab4e
Jens Zastrow <jens.zastrow_external@immobilienscout24.de>
2015-12-04 11:31:31 +0100

    remove logstash service spec

--- tests/spec/localhost/logstash_spec.rb
+++ tests/spec/localhost/logstash_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-describe service('logstash') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe file('/etc/logstash.conf') do
   it { should contain 'add_field => { "origin" => "ooooorigin" }' }
   it { should contain 'add_field => { "foo" => "bar" }' }
