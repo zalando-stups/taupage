33df6c6258b0e4b8b9feb8aa8865191367e45eb6
Jens Zastrow <jens.zastrow_external@immobilienscout24.de>
2015-12-14 17:30:30 +0100

    fix spec testing

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -2,6 +2,8 @@
 echo "Preparing Serverspec and running system tests..."
 # sudo apt-get update &&
 apt-get install -y ruby1.9.1
+gem install net-ssh -v 2.9.2 --no-ri --no-rdoc
+ 
 gem install bundler rake serverspec --no-ri --no-rdoc
 chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
