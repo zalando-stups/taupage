817e78e6323e149276713409412a8dc711d51d72
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-12-29 12:50:06 +0100

    adjust upgrade script, because of aws errors on build process

--- build/setup.d/10-upgrade-base.sh
+++ build/setup.d/10-upgrade-base.sh
@@ -4,7 +4,7 @@ set -x
 
 echo "Updating system..."
 
-sudo rm -rf /var/lib/apt/lists/*
+# sudo rm -rf /var/lib/apt/lists/*
 apt-get update -y  # -q >>/tmp/build/upgrade.log
 #apt-mark hold openssh-server
 #apt-get dist-upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y # -q  >>/tmp/build/upgrade.log
