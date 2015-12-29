8e190482ce58a6e3febed7d6c7a4de94df4b60e3
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-12-29 13:30:33 +0100

    change ruby servspec install because of previous install errors

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Preparing Serverspec and running system tests..."
 # sudo apt-get update &&
-apt-get install -y ruby1.9.1
-gem install bundler rake serverspec --no-ri --no-rdoc
+apt-get install -y ruby1.9.1 ruby-serverspec
+# gem install bundler rake serverspec --no-ri --no-rdoc
 chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
 rake
