526d048710b70bd82574b94b8fdcf986505181c4
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2015-12-29 12:36:31 +0100

    fix the public sharing script. Copy AMI to other regions

--- share-ami-public.sh
+++ share-ami-public.sh
@@ -19,39 +19,40 @@ imageid=$(aws ec2 describe-images --region $region --filters Name=tag-key,Values
 ami_name=$(aws ec2 describe-images --region $region --filters Name=tag-key,Values=Version Name=tag-value,Values=$TAUPAGE_VERSION --query 'Images[*].{ID:Name}' --output  text)
 commit_id=$(git log | head -n 1 | awk {'print $2'})
 
-
+#share AMI in default region
 for account in $all_accounts; do
-    echo "Sharing AMI with account $account ..."
+    echo "Sharing AMI in $region with account $account ..."
     aws ec2 modify-image-attribute --region $region --image-id $imageid --launch-permission "{\"Add\":[{\"UserId\":\"$account\"}]}"
+done
 
-    for target_region in $copy_regions; do
-        echo "Copying AMI to region $target_region ..."
-        result=$(aws ec2 copy-image --source-region $region --source-image-id $imageid --region $target_region --name $ami_name --description "$ami_description" --output json)
-        target_imageid=$(echo $result | jq .ImageId | sed 's/"//g')
+#copy ami to target regions
+for target_region in $copy_regions; do
+    echo "Copying AMI to region $target_region ..."
+    result=$(aws ec2 copy-image --source-region $region --source-image-id $imageid --region $target_region --name $ami_name --description "$ami_description" --output json)
+    target_imageid=$(echo $result | jq .ImageId | sed 's/"//g')
 
-        state="no state yet"
-        while [ true ]; do
-        echo "Waiting for AMI creation in $target_region ... ($state)"
+    state="no state yet"
+    while [ true ]; do
+    echo "Waiting for AMI creation in $target_region ... ($state)"
 
-        result=$(aws ec2 describe-images --region $target_region --output json --image-id $target_imageid)
-        state=$(echo $result | jq .Images\[0\].State | sed 's/"//g')
+    result=$(aws ec2 describe-images --region $target_region --output json --image-id $target_imageid)
+    state=$(echo $result | jq .Images\[0\].State | sed 's/"//g')
 
-        if [ "$state" = "failed" ]; then
-            echo "Image creation failed."
-            exit 1
-        elif [ "$state" = "available" ]; then
-            break
-        fi
+    if [ "$state" = "failed" ]; then
+        echo "Image creation failed."
+        exit 1
+    elif [ "$state" = "available" ]; then
+        break
+    fi
+    sleep 10
 
-        sleep 10
-        done
+    #create image tags in target region
+    aws ec2 create-tags --region $target_$region --resources $target_$imageid --tags Key=Version,Value=$TAUPAGE_VERSION
+    aws ec2 create-tags --region $target_$region --resources $target_$imageid --tags Key=CommitID,Value=$commit_id
 
-        for account in $all_accounts; do
-        echo "Sharing AMI with account $account ..."
+    # share image in target region
+    for account in $all_accounts; do
+        echo "Sharing AMI in $target_region with account $account ..."
         aws ec2 modify-image-attribute --region $target_region --image-id $target_imageid --launch-permission "{\"Add\":[{\"UserId\":\"$account\"}]}"
-        # set tags in other account
-        aws ec2 create-tags --region $target_$region --resources $target_$imageid --tags Key=Version,Value=$TAUPAGE_VERSION
-        aws ec2 create-tags --region $target_$region --resources $target_$imageid --tags Key=CommitID,Value=$commit_id
-        done
     done
 done
