4627be69770da7163fb9cb58b0d13b3257409e0d
Felix Mueller <felix.mueller@zalando.de>
2016-01-05 15:43:43 +0100

    Install ruby2.1

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -5,7 +5,7 @@ echo "Preparing Serverspec and running system tests..."
 # Install ruby
 add-apt-repository ppa:brightbox/ruby-ng-experimental
 apt-get update
-apt-get install ruby2.0
+apt-get install -y ruby2.1
 gem install bundler rake serverspec --no-ri --no-rdoc
 chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
