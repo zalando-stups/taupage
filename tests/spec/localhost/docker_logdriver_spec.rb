019f4fe67250aea0e8d192c81bc53168e2a7605d
László Károlyi <laszlo@karolyi.hu>
2016-01-07 18:45:04 +0100

    We don't necessarily look for ubuntu, the point is, docker runs

--- tests/spec/localhost/docker_logdriver_spec.rb
+++ tests/spec/localhost/docker_logdriver_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe command("docker inspect $(docker ps -a|tail -1|awk '{print $1}')") do
-  its(:stdout) { should contain '"Image": "ubuntu:14.04"' }
+  its(:stdout) { should contain '"Name": "/logstash"' }
 #  its(:stdout) { should contain '"Type": "gelf"' }
 #  its(:stdout) { should contain '"gelf-address": "udp://localhost:12201"' }
 end
