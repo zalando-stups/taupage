96fbfeabd68f78858de2e44f28e29360a2415cf1
László Károlyi <laszlo@karolyi.hu>
2016-01-07 18:20:23 +0100

    Trying to fix image id lookup

--- tests/spec/localhost/docker_logdriver_spec.rb
+++ tests/spec/localhost/docker_logdriver_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe command('docker inspect ubuntu:14.04') do
+describe command("docker inspect $(docker ps -a|tail -1|awk '{print $1}')") do
   its(:stdout) { should contain '"Image": "ubuntu:14.04"' }
 #  its(:stdout) { should contain '"Type": "gelf"' }
 #  its(:stdout) { should contain '"gelf-address": "udp://localhost:12201"' }
