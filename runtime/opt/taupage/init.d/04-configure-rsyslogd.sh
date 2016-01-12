ad035993e3e5fb29f25550dab1938be251e94a84
Felix Mueller <felix.mueller@zalando.de>
2016-01-12 11:09:01 +0100

    Fix for #169 - put  on top

--- runtime/opt/taupage/init.d/04-configure-rsyslogd.sh
+++ runtime/opt/taupage/init.d/04-configure-rsyslogd.sh
@@ -3,10 +3,10 @@
 eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
 
 rsyslog_max_message_size=$config_rsyslog_max_message_size
-rsyslog_config=/etc/rsyslog.d/19-messagesize.conf
+rsyslog_config=/etc/rsyslog.conf
 
 if [[ -n "$rsyslog_max_message_size" ]]; then
     echo "setting rsyslog_max_message_size..."
-    echo "\$MaxMessageSize ${rsyslog_max_message_size}" > $rsyslog_config
+    grep -q '^$MaxMessageSize' $rsyslog_config || sed -i "1s/^/\$MaxMessageSize ${rsyslog_max_message_size}\n/" $rsyslog_config
     service rsyslog restart
 fi
