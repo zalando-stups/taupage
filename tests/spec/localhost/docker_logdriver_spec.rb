2269b16be75c8b1abec02ebf44ca67c58337c0d0
mlefringhausen <martin.lefringhausen@immobilienscout24.de>
2016-01-12 14:45:20 +0100

    cloud-317 use test-container

--- tests/spec/localhost/docker_logdriver_spec.rb
+++ tests/spec/localhost/docker_logdriver_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe command("docker inspect $(docker ps -a|tail -1|awk '{print $1}')") do
-  its(:stdout) { should contain '"Name": "/logstash"' }
-#  its(:stdout) { should contain '"Type": "gelf"' }
-#  its(:stdout) { should contain '"gelf-address": "udp://localhost:12201"' }
+describe command("docker inspect privreg-test-docker") do
+  its(:stdout) { should contain '"Name": "/privreg-test-docker"' }
+  its(:stdout) { should contain '"Type": "gelf"' }
+  its(:stdout) { should contain '"gelf-address": "udp://localhost:12201"' }
 end
