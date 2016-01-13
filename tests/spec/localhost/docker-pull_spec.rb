a81440fa6922cbe557d058ecc42249f0df080898
Sebastian Spoerer <sebastian.spoerer@immobilienscout24.de>
2016-01-13 12:13:47 +0100

    need pull for testing

--- tests/spec/localhost/docker-pull_spec.rb
+++ tests/spec/localhost/docker-pull_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe command('docker images') do
-  #its(:stdout) { should contain 'privreg-test-docker' }
+  its(:stdout) { should contain 'privreg-test-docker' }
   its(:stdout) { should contain 'ecr-test-docker' }
 end
 
