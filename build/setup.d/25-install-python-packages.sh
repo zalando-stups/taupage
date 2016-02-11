45b6331db7dee8bc38802a5bef1e34a68f35ec93
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2016-02-11 16:06:28 +0100

    add pip3 install upgrade when installing python packages

--- build/setup.d/25-install-python-packages.sh
+++ build/setup.d/25-install-python-packages.sh
@@ -9,4 +9,4 @@ requests
 
 echo "Installing Python packages..."
 
-pip3 install --log-file=install_python_errors.log --log=install_python.log --exists-action i $pkgs
+pip3 install -U --log-file=install_python_errors.log --log=install_python.log --exists-action i $pkgs
