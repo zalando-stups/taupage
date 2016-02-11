214bc183c05663624f27644a4bc37226c47e527c
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2016-02-11 15:37:37 +0100

    remove python3 apt requests test

--- tests/spec/localhost/base-packages_spec.rb
+++ tests/spec/localhost/base-packages_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 basepackage = [ 'rsyslog-gnutls',
                 'python-setuptools',
-                'python3-requests',
                 'python3-yaml',
                 'python3-pip',
                 'python3-jinja2',
