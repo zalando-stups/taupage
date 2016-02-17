b15cc72b8e7548d7d4a7e51d111af5e8982ccbf7
Felix Mueller <felix.mueller@zalando.de>
2016-02-17 11:42:28 +0100

    Fix for CVE-2015-7547

--- build/setup.d/10-upgrade-base.sh
+++ build/setup.d/10-upgrade-base.sh
@@ -6,6 +6,7 @@ echo "Updating system..."
 
 # sudo rm -rf /var/lib/apt/lists/*
 apt-get update -y  # -q >>/tmp/build/upgrade.log
-#apt-mark hold openssh-server
+apt-mark hold openssh-server
+apt-get install --only-upgrade libc6
 #apt-get dist-upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y # -q  >>/tmp/build/upgrade.log
 #aptitude unhold openssh-server
