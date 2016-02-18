0b5606726ce044885bbdecceaf9b194083efa378
Felix Mueller <felix.mueller@zalando.de>
2016-02-18 09:40:52 +0100

    Added tests for etcd

---
+++ tests/spec/localhost/etcd_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+# Test if etcd port is listening
+describe port(2379) do
+  it { should be_listening.on('127.0.0.1').with('tcp') }
+end
+
+# Test if etcd process is running
+describe process("etcd") do
+  its(:args) { should match /etcd --advertise-client-urls/ }
+end
