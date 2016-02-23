abfa808e02460076391960dcf2b2b1035e42ea59
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2016-02-23 16:57:16 +0100

    we don't need the zalando ca certificates

--- build/setup.d/30-workaround-install-zalando-ca.sh
+++
@@ -1,5 +0,0 @@
-echo "Installing Zalando CA..."
-
-curl https://static.zalando.de/ca/zalando-service.ca > /usr/local/share/ca-certificates/zalando-service.crt
-curl https://static.zalando.de/ca/zalando-root.ca > /usr/local/share/ca-certificates/zalando-root.crt
-update-ca-certificates
