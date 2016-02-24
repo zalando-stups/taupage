1e60882a304072fdb210937a732a5c4982a6d06d
Felix Mueller <felix.mueller@zalando.de>
2016-02-24 13:54:40 +0100

    Fixes spam in various python scripts running taupage

--- runtime/usr/local/lib/python3.4/dist-packages/taupage/__init__.py
+++ runtime/usr/local/lib/python3.4/dist-packages/taupage/__init__.py
@@ -83,8 +83,7 @@ def get_default_port(config: dict):
 
 
 def configure_logging(level=logging.INFO):
-    logging.basicConfig(level=level, format='%(levelname)s: %(message)s')
-    logging.getLogger('urllib3.connectionpool').setLevel(logging.WARN)
+    logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.WARN)
 
 
 def is_sensitive_key(k):
