621f23fa1465b0f3f680318314cfb5950caba3e3
jzastrow <mail@jens-zastrow.de>
2016-03-03 11:08:12 +0100

    logging: do not create init script in setup, but use static one

--- build/setup.d/72-install-kinesis-logstash.sh
+++ build/setup.d/72-install-kinesis-logstash.sh
@@ -14,7 +14,7 @@ __EOF
   docker pull busybox
 }
 
-function createTagsToLogstashHelper {
+function createTagsToLogstashHelperScript {
   # store logstash to 
   cat <<__EOF > /bin/tags-to-logstash.sh
 #!/usr/bin/env python3
@@ -31,134 +31,6 @@ __EOF
   chmod +x /bin/tags-to-logstash.sh
 }
 
-function createLogstashUpstartService {
-  cat <<EOF > /etc/init/logstash.conf
-description "logstash"
-task
-start on filesystem and started docker
-stop on runlevel [!2345]
-
-script
-  set +e
-  RESULT=\$(docker inspect logstash)
-  if [ \$? = 0 ]; then
-    true
-  else
-    echo "/meta/taupage.yaml" | logger -t "kinesis-logstash"
-    cat /meta/taupage.yaml
-    
-    if [ "\$(cat /meta/taupage.yaml | shyaml get-value 'logstash.enabled' 'false')" = "false" ]; then
-      echo "logstash not enabled." | logger -t "kinesis-logstash"
-      echo "finished"  | logger -t "kinesis-logstash"
-      exit 0
-    fi
-    
-    instanceId=\$(ec2metadata --instance-id)
-    instanceAvailabilityZone=\$(ec2metadata --availability-zone)
-    instanceRegion=\$(echo \${instanceAvailabilityZone} | rev | cut -c 2- | rev)
-
-    region=\$(cat /meta/taupage.yaml | shyaml get-value "logstash.kinesis_region" "\${instanceRegion}")
-    stream=\$(cat /meta/taupage.yaml | shyaml get-value "logstash.kinesis_stream" "logging")
-
-    rm -rf /etc/logstash.conf
-    cat <<__EOF > /etc/logstash.conf
-input {
-  # Graylog Extended Log Format - https://www.graylog.org/resources/gelf/
-  # https://www.elastic.co/guide/en/logstash/current/plugins-inputs-gelf.html
-  gelf {
-    # listen to udp://0.0.0.0:12201
-  }
-}
-
-filter {
-  # rename 'short_message' to 'message'
-  if [short_message] {
-    mutate {
-      rename => { "short_message" => "message" }
-    }
-  }
-}
-
-# extract json from field 'message'
-filter {
-   grok {
-      match => ["message", "(?<jsonMessage>^{.*}$)"]
-      tag_on_failure => []
-   }
-}
-
-filter {
-  # remove 'message' when jsonMessage found
-  if [jsonMessage] {
-    mutate {
-      remove_field => ["message"]
-    }
-  }
-  
-  # try to import json from field 'jsonMessage'
-  json {
-    # https://www.elastic.co/guide/en/logstash/current/plugins-filters-json.html
-    source => "jsonMessage"
-    remove_field => "jsonMessage"
-  }
-}
-
-filter {
-  # add instanceId, availabilityZone, region
-  mutate {
-    add_field => { "instance_id" => "\${instanceId}" }
-    add_field => { "availability_zone" => "\${instanceAvailabilityZone}" }
-    add_field => { "region" => "\${region}" }
-__EOF
-
-  /bin/tags-to-logstash.sh >> /etc/logstash.conf
-
-cat <<__EOF >> /etc/logstash.conf
-  }
-}
-
-# force field 'level' to be a string
-filter {
-  mutate {
-    convert => { "level" => "string" }
-  }
-}
-
-output {
-  # https://github.com/samcday/logstash-output-kinesis
-  kinesis {
-    stream_name => "\${stream}"
-    region => "\${region}"
-    # for more settings see
-    # https://github.com/awslabs/amazon-kinesis-producer/blob/v0.10.0/java/amazon-kinesis-producer/src/main/java/com/amazonaws/services/kinesis/producer/KinesisProducerConfiguration.java#L230
-    metrics_level => "none"
-    aggregation_enabled => false
-    randomized_partition_key => true
-  }
-}
-__EOF
-
-    docker run \
-      -d \
-      --restart=always \
-      --name logstash \
-      -p 12201:12201/udp \
-      -v /etc/logstash.conf:/logstash.conf \
-      ${logstashImage} \
-      logstash -f /logstash.conf
-    
-    echo "wait for amazon-kinesis-producer-native-binaries to be started"
-    until docker logs logstash | grep -m 1 "${stream}"; do echo -n "." | logger -t "kinesis-logstash"; docker run --rm --log-driver=gelf --log-opt gelf-address=udp://localhost:12201 busybox echo "dummy"; sleep 1; done
-  fi
-  echo "finished"  | logger -t "kinesis-logstash"
-end script
-EOF
-}
-
 pip install shyaml
 buildLogstashDockerContainer
-createTagsToLogstashHelper
-createLogstashUpstartService
-
-# testing
-# docker kill logstash || true; docker rm logstash || true; rm -rf /var/log/upstart/logstash.log && service logstash start && sleep 2 && less /var/log/upstart/logstash.log
+createTagsToLogstashHelperScript
