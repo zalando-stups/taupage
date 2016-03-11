a75859b50118b1206e64bbd11f3c1afc053ecf24
Christian Lohmann <christian.lohmann@zalando.de>
2016-03-11 13:57:24 +0100

    #187 added overwrite support for appdynamics access key and globalname also

--- runtime/opt/taupage/init.d/85-setup-appdynamics.sh
+++ runtime/opt/taupage/init.d/85-setup-appdynamics.sh
@@ -8,6 +8,28 @@ if [ -z "$config_appdynamics_application" ]; then
 	exit 0
 fi
 
+# checking for multi-tenant support.
+# All 3 properties (appdynamics_account_name, appdynamics_account_globalname, appdynamics_account_globalname)
+# must be set for multi-tenancy in taupage.yaml
+if [ -n "$config_appdynamics_account_name" ]; then
+    if [ -n "$config_appdynamics_account_access_key" ]; then
+        if [ -n "$config_appdynamics_account_globalname" ]; then
+            # we have to overwrite the default account settings in the analytics agent props file
+            properties_file="/opt/proprietary/appdynamics-machine/monitors/analytics-agent/conf/analytics-agent.properties"
+            sed -i "1,$ s/http.event.accountName/http.event.accountName=$config_appdynamics_account_globalname/" $properties_file
+            sed -i "1,$ s/http.event.accessKey/http.event.accessKey=$config_appdynamics_account_access_key/" $properties_file
+        else
+            echo "ERROR: AppDynamics Multi Tenancy for account $config_appdynamics_account_name was detected \n
+                but appdynamics_account_globalname wasn't set. Please provide the proper appdynamics_account_globalname!"
+            exit 1
+        fi
+    else
+        echo "ERROR: AppDynamics Multi Tenancy for account $config_appdynamics_account_name was detected \n
+            but appdynamics_account_access_key wasn't set. Please provide the proper account_access_key!"
+        exit 1
+    fi
+fi
+
 # node name has to be unique across the whole ecosystem
 node="${config_application_id}_${config_application_version}_$(hostname)"
 
@@ -19,6 +41,10 @@ cat /opt/proprietary/appdynamics-configs | while read conf; do
 	if [ -n "$config_appdynamics_account_name" ]; then
 	    sed -i "1,$ s/<account-name.*$/<account-name>$config_appdynamics_account_name<\/account-name>/" $conf
     fi
+    if [ -n "$config_appdynamics_account_access_key" ]; then
+	    sed -i "1,$ s/<account-access-key.*$/<account-access-key>$config_appdynamics_account_access_key<\/account-access-key>/" $conf
+    fi
+
 	sed -i "1,$ s/APPDYNAMICS_APPLICATION/$config_appdynamics_application/" $conf
 	sed -i "1,$ s/APPDYNAMICS_TIER/$config_application_id/" $conf
 	sed -i "1,$ s/APPDYNAMICS_NODE/$node/" $conf
@@ -28,31 +54,31 @@ cat /opt/proprietary/appdynamics-configs | while read conf; do
 	echo $node > $agent_dir/uniqueHostId
 done
 
-   # configure application.log & syslog
-   # first "hack" this should be configurable over the taupage.yaml file.
-   application_log_job="/opt/proprietary/appdynamics-machine/monitors/analytics-agent/conf/job/application-log.job"
-   syslog_job="/opt/proprietary/appdynamics-machine/monitors/analytics-agent/conf/job/syslog.job"
+# configure application.log & syslog
+# first "hack" this should be configurable over the taupage.yaml file.
+application_log_job="/opt/proprietary/appdynamics-machine/monitors/analytics-agent/conf/job/application-log.job"
+syslog_job="/opt/proprietary/appdynamics-machine/monitors/analytics-agent/conf/job/syslog.job"
 
-   #enable application.log job
-   if [ -f $application_log_job ]; then
-      sed -i "1,$ s/enabled.*$/enabled: true/" $application_log_job
-      sed -i "1,$ s/APPLICATION_ID/$config_application_id/" $application_log_job
-      sed -i "1,$ s/APPLICATION_VERSION/$config_application_version/" $application_log_job
-      sed -i "1,$ s/APPDYNAMICS_NODE/$node/" $application_log_job
-   else
-      echo "INFO: application_job file doesn't exists, skipping setup"
-   fi
+#enable application.log job
+if [ -f $application_log_job ]; then
+  sed -i "1,$ s/enabled.*$/enabled: true/" $application_log_job
+  sed -i "1,$ s/APPLICATION_ID/$config_application_id/" $application_log_job
+  sed -i "1,$ s/APPLICATION_VERSION/$config_application_version/" $application_log_job
+  sed -i "1,$ s/APPDYNAMICS_NODE/$node/" $application_log_job
+else
+  echo "INFO: application_job file doesn't exists, skipping setup"
+fi
 
-   # enable syslogjob
-   if [ -f $syslog_job ]; then
-      sed -i "1,$ s/enabled.*$/enabled: true/" $syslog_job
-      sed -i "1,$ s/APPLICATION_ID/$config_application_id/" $syslog_job
-      sed -i "1,$ s/APPLICATION_VERSION/$config_application_version/" $syslog_job
-      sed -i "1,$ s/APPDYNAMICS_NODE/$node/" $syslog_job
+# enable syslogjob
+if [ -f $syslog_job ]; then
+  sed -i "1,$ s/enabled.*$/enabled: true/" $syslog_job
+  sed -i "1,$ s/APPLICATION_ID/$config_application_id/" $syslog_job
+  sed -i "1,$ s/APPLICATION_VERSION/$config_application_version/" $syslog_job
+  sed -i "1,$ s/APPDYNAMICS_NODE/$node/" $syslog_job
 
-   else
-      echo "INFO: syslog_job file doesn't exists, skipping setup"
-   fi
+else
+  echo "INFO: syslog_job file doesn't exists, skipping setup"
+fi
 
 
 # start machine agent
