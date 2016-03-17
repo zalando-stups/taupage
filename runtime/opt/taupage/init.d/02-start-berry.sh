9225daf334aaf81cbb2bc6de0b497a75556c1793
Christian Lohmann <christian.lohmann@zalando.de>
2016-03-17 10:14:11 +0100

    #193 added ERROR: tag to echo output, added the credentials dir argument to berry command

--- runtime/opt/taupage/init.d/02-start-berry.sh
+++ runtime/opt/taupage/init.d/02-start-berry.sh
@@ -4,11 +4,11 @@
 grep 'mint_bucket' /meta/taupage.yaml
 if [ $? -eq 0 ]; then
     # start berry once to make sure to have valid credentials for the rest of taupage init
-    berry --once
+    berry /meta/credentials --once
     # then we are fine to move it to the background
     service berry start
 else
-    echo "No mint_bucket entry in taupage.yaml"
+    echo "ERROR: No mint_bucket entry in taupage.yaml"
     exit 1
 fi
 exit 0
