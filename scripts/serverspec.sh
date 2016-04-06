cddb591fbad2938ac25721903428f96cb10fae06
Felix Mueller <felix.mueller@zalando.de>
2016-04-06 09:41:46 +0200

    Install ruby-dev

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -5,7 +5,7 @@ echo "Preparing Serverspec and running system tests..."
 # Install ruby
 add-apt-repository ppa:brightbox/ruby-ng-experimental
 apt-get update
-apt-get install -y ruby2.1
+apt-get install -y ruby2.1 ruby-dev
 gem install bundler rake serverspec --no-ri --no-rdoc
 chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
