b667d2e282f693eadf3555c3406f23cf2a7585f2
Felix Mueller <felix.mueller@zalando.de>
2016-05-02 17:35:00 +0200

    Roll back PR #219

--- runtime/opt/taupage/bin/push-audit-logs.py
+++ runtime/opt/taupage/bin/push-audit-logs.py
@@ -11,15 +11,11 @@ import requests
 import sys
 import time
 
-from taupage import configure_logging, get_config, get_boot_time
-from base64 import b64encode
+from taupage import configure_logging, get_config, get_token, get_boot_time
 
 
 def push_audit_log(config: dict, instance_logs_url, account_id, region, instance_id, boot_time, fn, compress=False):
-    userAndPass = b64encode(bytes('{}:{}'.format(
-            config.get('logsink_username'),
-            config.get('logsink_password')),
-            encoding='ascii')).decode("ascii") or ''
+    token = get_token(config, 'taupage', ['uid']) or {}
 
     with open(fn, 'rb') as fd:
         contents = fd.read()
@@ -35,7 +31,7 @@ def push_audit_log(config: dict, instance_logs_url, account_id, region, instance
     try:
         response = requests.post(instance_logs_url, data=json.dumps(data),
                                  headers={'Content-Type': 'application/json',
-                                          'Authorization': 'Basic {}'.format(userAndPass)})
+                                          'Authorization': 'Bearer {}'.format(token.get('access_token'))})
         if response.status_code == 201:
             os.remove(fn)
         else:
--- runtime/opt/taupage/init.d/03-push-taupage-yaml.py
+++ runtime/opt/taupage/init.d/03-push-taupage-yaml.py
@@ -7,8 +7,7 @@ import logging
 import requests
 import yaml
 
-from taupage import configure_logging, get_config, mask_dictionary, get_boot_time
-from base64 import b64encode
+from taupage import configure_logging, get_config, mask_dictionary, get_token, get_boot_time
 
 
 def main():
@@ -18,10 +17,7 @@ def main():
     instance_logs_url = config.get('instance_logs_url')
 
     if instance_logs_url:
-        userAndPass = b64encode(bytes('{}:{}'.format(
-                config.get('logsink_username'),
-                config.get('logsink_password')),
-                encoding='ascii')).decode("ascii") or ''
+        token = get_token(config, 'taupage', ['uid']) or {}
 
         # identity = {'region': 'eu-west-1', 'accountId': 123456, 'instanceId': 'i-123'}
         identity = boto.utils.get_instance_identity()['document']
@@ -47,7 +43,7 @@ def main():
             # TODO: use OAuth credentials
             response = requests.post(instance_logs_url, data=json.dumps(data), timeout=5,
                                      headers={'Content-Type': 'application/json',
-                                              'Authorization': 'Basic {}'.format(userAndPass)})
+                                              'Authorization': 'Bearer {}'.format(token.get('access_token'))})
             if response.status_code != 201:
                 logging.warn('Failed to push Taupage YAML: server returned HTTP status {}: {}'.format(
                     response.status_code,
