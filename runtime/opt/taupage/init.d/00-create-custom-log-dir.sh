392142936ecf1b9f9615c7d56ef7f736d2026ab1
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2016-06-02 11:27:33 +0200

    change permission on /var/log-custom that docker is able to write logs

--- runtime/opt/taupage/init.d/00-create-custom-log-dir.sh
+++ runtime/opt/taupage/init.d/00-create-custom-log-dir.sh
@@ -5,3 +5,6 @@ if [ ! -d /var/log-custom ]
 then
   mkdir /var/log-custom
 fi
+
+#make writeable for application user that are docker is able to write custom logs
+chown application.application /var/log-custom
