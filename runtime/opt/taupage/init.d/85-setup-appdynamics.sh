9492302efbe7b219efcbf762a2e6bc985d2aafb4
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2016-08-25 14:47:10 +0200

    set Fields Tiername and AppDynamics Applicationname in the default log jobfiles.

--- runtime/opt/taupage/init.d/85-setup-appdynamics.sh
+++ runtime/opt/taupage/init.d/85-setup-appdynamics.sh
@@ -109,6 +109,8 @@ if [ -f $application_log_job ]; then
     sed -i "1,$ s/APPLICATION_VERSION/$config_application_version/" $application_log_job
     sed -i "1,$ s/APPDYNAMICS_NODE/$node/" $application_log_job
     sed -i "1,$ s/APPDYNAMICS_STACKNAME/$STACK_NAME/" $application_log_job
+    sed -i "1,$ s/APPDYNAMICS_APPLICATION/$APPLICATIONNAME/" $application_log_job
+    sed -i "1,$ s/APPDYNAMICS_TIERNAME/$config_application_id/" $application_log_job
 else
     echo "INFO: application_job file doesn't exists, skipping setup"
 fi
@@ -120,6 +122,8 @@ if [ -f $syslog_job ]; then
     sed -i "1,$ s/APPLICATION_VERSION/$config_application_version/" $syslog_job
     sed -i "1,$ s/APPDYNAMICS_NODE/$node/" $syslog_job
     sed -i "1,$ s/APPDYNAMICS_STACKNAME/$STACK_NAME/" $syslog_job
+		sed -i "1,$ s/APPDYNAMICS_APPLICATION/$APPLICATIONNAME/" $syslog_job
+		sed -i "1,$ s/APPDYNAMICS_TIERNAME/$config_application_id/" $syslog_job
 else
   echo "INFO: syslog_job file doesn't exists, skipping setup"
 fi
