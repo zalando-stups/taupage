890fc7ca1fa95224482034fa7cf7c877064667af
Evan Lezar <evan.lezar@zalando.de>
2016-10-20 10:43:42 +0200

    Add md5 check to CUDA install

--- build/setup.d/35-install-nvidia-driver.sh
+++ build/setup.d/35-install-nvidia-driver.sh
@@ -6,6 +6,15 @@ echo "Installing NVIDIA driver"
 
 # Use the CUDA repo instead of the run file.
 wget -P /tmp http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+
+REQUIRED_MD5_SUM="e810ded23efe35e3db63d2a92288f922"
+ACTUAL_MD5_SUM=$(md5sum /tmp/cuda-repo-ubuntu1404_7.5-18_amd64.deb)
+if [ x"$ACTUAL_MD5_SUM" -ne x"$REQUIRED_MD5_SUM" ]; then
+	echo "MD5 Mismatch"
+	echo "Got $ACTUAL_MD5_SUM (expecting $REQUIRED_MD5_SUM)"
+	exit 1
+fi
+
 dpkg -i /tmp/cuda-repo-ubuntu1404_7.5-18_amd64.deb
 
 apt-get update
