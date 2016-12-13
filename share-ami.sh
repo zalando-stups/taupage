c95bc36611223abe0bdc7493b4d73a2de22897c1
Matthias Kerk <matthias.kerk@zalando.de>
2016-12-13 17:39:08 +0100

    add all_accounts and fix pipeline process

--- share-ami.sh
+++ share-ami.sh
@@ -9,26 +9,28 @@ cd $(dirname $0)
 . $CONFIG_FILE
 
 # get ami_id, ami_name and commitID
-imageid=$(aws ec2 describe-images --region $region --filters Name=tag-key,Values=Version Name=tag-value,Values=$TAUPAGE_VERSION --query 'Images[*].{ID:ImageId}' --output  text)
-ami_name=$(aws ec2 describe-images --region $region --filters Name=tag-key,Values=Version Name=tag-value,Values=$TAUPAGE_VERSION --query 'Images[*].{ID:Name}' --output  text)
-commit_id=$(git log | head -n 1 | awk {'print $2'})
+result=$(aws ec2 describe-images --region $region --filters Name=tag-key,Values=Version Name=tag-value,Values=$TAUPAGE_VERSION --query 'Images[0]' --output json)
+imageid=$(echo $result | jq -r '.ImageId')
+ami_name=$(echo $result | jq -r '.Name')
+commit_id=$(git rev-parse HEAD)
 
+#share AMI in default region
 for account in $accounts; do
     echo "Sharing AMI with account $account ..."
-    aws ec2 modify-image-attribute --region $region --image-id $imageid --launch-permission "{\"Add\":[{\"UserId\":\"$account\"}]}"
+    aws ec2 modify-image-attribute --region $region --image-id $imageid --launch-permission "Add=[{UserId:$account}]"
 done
 
 for target_region in $copy_regions; do
     echo "Copying AMI to region $target_region ..."
     result=$(aws ec2 copy-image --source-region $region --source-image-id $imageid --region $target_region --name $ami_name --description "$ami_description" --output json)
-    target_imageid=$(echo $result | jq .ImageId | sed 's/"//g')
+    target_imageid=$(echo $result | jq -r '.ImageId')
 
     state="no state yet"
     while [ true ]; do
         echo "Waiting for AMI creation in $target_region ... ($state)"
 
         result=$(aws ec2 describe-images --region $target_region --output json --image-id $target_imageid)
-        state=$(echo $result | jq .Images\[0\].State | sed 's/"//g')
+        state=$(echo $result | jq -r '.Images[0].State')
 
         if [ "$state" = "failed" ]; then
             echo "copying Image failed."
@@ -42,24 +44,20 @@ for target_region in $copy_regions; do
 
     for account in $accounts; do
         echo "Sharing AMI with account $account ..."
-        aws ec2 modify-image-attribute --region $target_region --image-id $target_imageid --launch-permission "{\"Add\":[{\"UserId\":\"$account\"}]}"
+        aws ec2 modify-image-attribute --region $target_region --image-id $target_imageid --launch-permission "Add=[{UserId:$account}]"
     done
 
     # set tags in other account
-    aws ec2 create-tags --region $target_$region --resources $target_$imageid --tags Key=Version,Value=$TAUPAGE_VERSION
-    aws ec2 create-tags --region $target_$region --resources $target_$imageid --tags Key=CommitID,Value=$commit_id
+    aws ec2 create-tags --region $target_$region --resources $target_imageid --tags "Key=Version,Value=$TAUPAGE_VERSION Key=CommitID,Value=$commit_id"
 done
 
 #check if image creation/copy was successfull
 if [ "$state" = "available" ]; then
-  #git add new release tag
-  # get AMI-name
-  ami_name=$(aws ec2 describe-images --region $region --filters Name=image-id,Values=$imageid --query 'Images[*].{ID:Name}' --output text)
-  git tag $ami_name
-  git push --tags
-  #tag image in Frankfurt with commitID
-  commit_id=$(git log | head -n 1 | awk {'print $2'})
-  aws ec2 create-tags --region $region --resources $imageid --tags Key=CommitID,Value=$commit_id
+    # git add new release tag
+    git tag $ami_name
+    git push --tags
+    #tag image in Frankfurt with commitID
+    aws ec2 create-tags --region $region --resources $imageid --tags Key=CommitID,Value=$commit_id
 else
-  echo "Image creation/copy failed."
+    echo "Image creation/copy failed."
 fi
