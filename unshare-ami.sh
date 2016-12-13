1d46a769d325ed6c10485e95f26835336a42c262
Matthias Kerk <matthias.kerk@zalando.de>
2016-12-13 17:38:04 +0100

    add unshare script

---
+++ unshare-ami.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CONFIG_FILE=$1
+TAUPAGE_VERSION=$2
+
+cd $(dirname $0)
+
+# load configuration file
+. $CONFIG_FILE
+
+# get ami_id, ami_name and commitID
+imageid=$(aws ec2 describe-images --region $region --filters Name=tag-key,Values=Version Name=tag-value,Values=$TAUPAGE_VERSION --query 'Images[*].{ID:ImageId}' --output  text)
+
+#share AMI in default region
+for account in $(aws ec2 describe-image-attribute --region $region --image-id $imageid --attribute launchPermission --query 'LaunchPermissions[]' --output text); do
+    echo "Unsharing AMI in $region with account $account ..."
+    aws ec2 modify-image-attribute --region $region --image-id $imageid --launch-permission "Remove=[{UserId:$account}]"
+done
+
+#copy ami to target regions
+for target_region in $copy_regions; do
+    # share image in target region
+    for account in $all_accounts; do
+        echo "Unsharing AMI in $target_region with account $account ..."
+        aws ec2 modify-image-attribute --region $target_region --image-id $target_imageid --launch-permission "Remove=[{UserId:$account}]"
+    done
+done
