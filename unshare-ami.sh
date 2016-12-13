9f02aed3c57051130dbc119b0ba5e209517c9af4
Matthias Kerk <matthias.kerk@zalando.de>
2016-12-13 19:44:15 +0100

    fix Output

--- unshare-ami.sh
+++ unshare-ami.sh
@@ -13,7 +13,7 @@ imageid=$(aws ec2 describe-images --region $region --filters Name=tag-key,Values
 shared_accounts=$(aws ec2 describe-image-attribute --region $region --image-id $imageid --attribute launchPermission --query 'LaunchPermissions[]' --output text)
 
 # unshare AMI in default region
-echo "Share AMI $imageid for $shared_accounts"
+echo "Unshare AMI $imageid for $shared_accounts"
 echo $shared_accounts | xargs aws ec2 modify-image-attribute --region $region --image-id $imageid --attribute launchPermission --operation-type remove --user-ids
 aws ec2 create-tags --region $region --resources $imageid --tags "Key=Shared,Value=None"
 
@@ -22,7 +22,7 @@ for target_region in $copy_regions; do
     # get ami_id
     target_imageid=$(aws ec2 describe-images --region $target_region --filters Name=tag-key,Values=Version Name=tag-value,Values=$TAUPAGE_VERSION --query 'Images[*].{ID:ImageId}' --output  text)
     shared_accounts=$(aws ec2 describe-image-attribute --region $target_region --image-id $target_imageid --attribute launchPermission --query 'LaunchPermissions[]' --output text)
-    echo "Share AMI $target_imageid for $shared_accounts"
+    echo "Unshare AMI $target_imageid for $shared_accounts"
     echo $shared_accounts | xargs aws ec2 modify-image-attribute --region $target_region --image-id $target_imageid --attribute launchPermission --operation-type remove --user-ids
     aws ec2 create-tags --region $target_region --resources $target_imageid --tags "Key=Shared,Value=None"
 done
