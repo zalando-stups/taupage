5039d71dbceb0a1e63ed85ca21300cffad368b30
Matthias Kerk <matthias.kerk@zalando.de>
2016-12-15 12:32:53 +0100

    add docker key and move logentries.list to runtime/etc/apt/sources.list.d

--- build/setup.d/05-add-repository-keys.sh
+++ build/setup.d/05-add-repository-keys.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-# Docker repository key
-apt_keys="
-58118E89F3A912897C070ADBF76221572C52609D
-"
-
-local_keys="
-newrelic.key
-logentries.key
-yandex.key
-docker.key
-scalyr.asc
-"
-
-
 echo "Adding repository keys..."
 
-for key in $apt_keys; do
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $key
-done
+find keys/ -type f -ls -exec apt-key add '{}' \;
 
-for key in $local_keys; do
-    apt-key add keys/$key
-done
-
-echo 'deb http://rep.logentries.com/ trusty main' > /etc/apt/sources.list.d/logentries.list
-
-# add Newrelic repo
-# echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' >> /etc/apt/sources.list.d/newrelic.list
-# wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
