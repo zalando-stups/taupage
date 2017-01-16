5fab8c3fe72ec6cbc6d77200ccda47448fc3c002
Sebastian Spoerer <sebastian@programmieramt.de>
2017-01-16 11:36:22 +0100

    taupage docker changed

--- tests/spec/localhost/docker-pull_spec.rb
+++ tests/spec/localhost/docker-pull_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe command('docker images') do
-  its(:stdout) { should contain 'privreg-test-docker' }
-  its(:stdout) { should contain 'ecr-test-docker' }
+  its(:stdout) { should contain 'taupage-do-not-delete' }
 end
 
