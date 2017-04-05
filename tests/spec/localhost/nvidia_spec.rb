8d187e6838fa4733f7ec9d2375690600248ce289
Jan Ulferts <jan.ulferts@zalando.de>
2017-04-05 17:29:25 +0200

    improved nvidia testing

--- tests/spec/localhost/nvidia_spec.rb
+++ tests/spec/localhost/nvidia_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe command('nvidia-docker run --rm nvidia/cuda nvidia-smi') do
+has_nvidiactl = file('/dev/nvidiactl').exists?
+
+# should always be installed
+describe file('/usr/bin/nvidia-docker') do
+  it { should exist }
+end
+
+# we just can test this on gpu enabled instances.
+describe command('nvidia-docker run --rm nvidia/cuda nvidia-smi'), :if => has_nvidiactl do
   its(:stdout) { should contain /Tesla K80/ }
 end
