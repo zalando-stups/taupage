3f68bd70bb06065d2c7dbffee0d6b3d61c8c80a3
Konrad Hosemann <konrad@hosemann.name>
2017-05-05 12:26:03 +0200

    ensure dockerd shutdown wait long enough for graceful container shutdown

--- build/setup.d/90-patch-docker-init.sh
+++ build/setup.d/90-patch-docker-init.sh
@@ -1,5 +1,5 @@
 # this ugly patching ensures that the shutdown of the docker daemon honours an increased
 # stop timeout of the container. Otherwise upstart will kill
 
-sed -i -e "s/stop on runlevel \[!2345\]/stop on starting rc RUNLEVEL=\[016\]/" docker.conf
-sed -i -e "s/kill timeout 20/kill timeout 120/" docker.conf
+sed -i -e "s/stop on runlevel \[!2345\]/stop on starting rc RUNLEVEL=\[016\]/" /etc/init/docker.conf
+sed -i -e "s/kill timeout 20/kill timeout 120/" /etc/init/docker.conf
