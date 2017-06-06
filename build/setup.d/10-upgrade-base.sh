4b679fbe7371600b51b7ce2ecb72b0bf7a7e0363
pluralistix <christian.weigmann@scout24.com>
2017-06-06 16:48:26 +0200

    fix problems with ssh via nc and a possible openssh-server restart after upgrade

--- build/setup.d/10-upgrade-base.sh
+++ build/setup.d/10-upgrade-base.sh
@@ -1,4 +1,11 @@
 echo "Updating system..."
 
 apt-get update -y  # -q >>/tmp/build/upgrade.log
-apt-get dist-upgrade -y # -q >>/tmp/build/upgrade.log
+
+#do not restart services on dist-upgrade, because the way we connect to ec2 via ssh and proxycommand nc get's broken if openssh-server get's updatet (and restartet)
+#as of now we do not expect the file to be present. Thus we stop here, if it is.
+[ -f /usr/sbin/policy-rc.d ] && exit 1
+echo -e '#!/bin/sh\n\nexit 101' >/usr/sbin/policy-rc.d
+chmod +x /usr/sbin/policy-rc.d
+apt-get dist-upgrade -y
+rm /usr/sbin/policy-rc.d
