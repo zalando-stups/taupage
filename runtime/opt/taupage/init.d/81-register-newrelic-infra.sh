5d19cefc88e694598d7107d446c6fe13e1b44fad
Jens Zastrow <jens.zastrow_external@immobilienscout24.de>
2017-06-09 09:36:10 +0200

    SIM-68 since nowbody @is24 uses the current newrelic impl in taupage, replace the old newrelic-sysmond with latest newrelic-infra agent.

---
+++ runtime/opt/taupage/init.d/81-register-newrelic-infra.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# read /etc/taupage.yaml
+# get NewRelic Key and register the agent
+
+#read taupage.yaml file
+eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
+
+#set more readable variables
+ACCOUNTKEY=$config_newrelic_account_key
+newrelic_config=/etc/newrelic.infra.yml
+
+#if NewRelic account exists in the yaml file. Register the NewRelic Daemon to this Account
+if [ -n "$ACCOUNTKEY" ];
+then
+    echo "license_key: $ACCOUNTKEY" > $newrelic_config
+    echo -n "Starting newrelic-infra ... ";
+    service newrelic-infra stop || true
+    service newrelic-infra start
+    if [ $? -eq 0 ];
+    then
+        echo -n "DONE"
+    else
+        echo -n "ERROR: Failed to start newrelic-infra!";
+        exit;
+    fi
+else
+    echo "INFO: NewRelic is not configured; skipping daemon setup.";
+    exit;
+fi
