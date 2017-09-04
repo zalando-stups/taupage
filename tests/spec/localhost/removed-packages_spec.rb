89f00714482c4ce9a4db37f885ddb59b39b3e38a
Sergey Novikov <snov@snov.me>
2017-09-04 18:57:22 +0200

    Fix spec filename

---
+++ tests/spec/localhost/removed-packages_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+packages = [
+	'build-essential',
+	'g++',
+	'g++-4.8',
+	'gcc',
+	'gcc-4.8',
+	'laptop-detect'
+]
+
+packages.each do |p|
+  describe package(p) do
+    it { should_not be_installed }
+  end
+end
