3e4e19c966bc7e4ced554e199a5972ed55a72344
Rajat Parida <rajat.parida@zalando.de>
2017-09-26 16:13:21 +0200

    Corrected setting instana key env variable

--- runtime/opt/taupage/init.d/80-setup-instana-agent.sh
+++ runtime/opt/taupage/init.d/80-setup-instana-agent.sh
@@ -6,16 +6,16 @@ eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
 #Set instana environment variables#
 #export INSTANA_AGENT_HOST=$config_instana_agent_host
 #export INSTANA_AGENT_PORT=$config_instana_agent_port
-export INSTANA_AGENT_KEY=$config_instana_agent_key
-
-# If KMS encrypted, decrypt KMS and save to INSTANA_AGENT_KEY variable
-if [! -z "${INSTANA_AGENT_KEY}"] ; then
+# Set INSTANA_AGENT_KEY as ENV variable. If KMS encrypted, decrypt KMS and save to INSTANA_AGENT_KEY variable
+if [! -z "${config_instana_agent_key}"] ; then
+  INSTANA_AGENT_KEY=${config_instana_agent_key}
   if [[ $INSTANA_AGENT_KEY == "aws:kms:"* ]]; then
   	ACCOUNTKEY=${INSTANA_AGENT_KEY##aws:kms:}
   	ACCOUNTKEY=`python3 /opt/taupage/bin/decrypt-kms.py $ACCOUNTKEY`
   	#overwrite INSTANA_AGENT_KEY with decrypted value
   	INSTANA_AGENT_KEY=$ACCOUNTKEY
   fi
+  export INSTANA_AGENT_KEY=$INSTANA_AGENT_KEY
 else
   echo "INFO: Instana access key is missing. Skipping Instana setup."
   exit 1
@@ -23,7 +23,7 @@ fi
 
 #Set instana zone for application -- e.g. AWS account alias
 if [! -z "${config_instana_zone}"] ; then
-  export INSTANA_ZONE=config_instana_zone
+  export INSTANA_ZONE=${config_instana_zone}
 else
   echo "INFO: Instana zone configuration is missing. Skipping Instana setup."
   exit 1
@@ -37,9 +37,9 @@ else
 # GET the INFRA/APM mode from environment variable
 if [! -z "${config_instana_agent_mode}"]; then
     shopt -s nocasematch
-  if [[$config_instana_agent_mode =~ "APM"]]; then
+  if [[${config_instana_agent_mode} =~ "APM"]]; then
     AGENTMODE="APM"
-  elif [[ $config_instana_agent_mode =~ "OFF" ]]; then
+  elif [[${config_instana_agent_mode} =~ "OFF" ]]; then
     AGENTMODE="OFF"
   else
     echo "INFO: Setting instana agent mode to INFRASTRUCTURE."
