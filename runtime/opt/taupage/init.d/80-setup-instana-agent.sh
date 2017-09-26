f91a8830545c94d24b2914ee31e7e00a458d7cad
Andreas Pfeiffer <andreas.pfeiffer@zalando.de>
2017-09-26 16:23:58 +0200

    add readable variables.

--- runtime/opt/taupage/init.d/80-setup-instana-agent.sh
+++ runtime/opt/taupage/init.d/80-setup-instana-agent.sh
@@ -2,13 +2,16 @@
 
 # read global taupage config
 eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
+INSTANA_AGENT_KEY=${config_instana_agent_key}
+INSTANA_ZONE=${config_instana_zone}
+INSTANA_TAGS=$config_instana_tags
+AGENTMODE=${config_instana_agent_mode}
 
 #Set instana environment variables#
 #export INSTANA_AGENT_HOST=$config_instana_agent_host
 #export INSTANA_AGENT_PORT=$config_instana_agent_port
 # Set INSTANA_AGENT_KEY as ENV variable. If KMS encrypted, decrypt KMS and save to INSTANA_AGENT_KEY variable
-if [! -z "${config_instana_agent_key}"] ; then
-  INSTANA_AGENT_KEY=${config_instana_agent_key}
+if [! -z "$INSTANA_AGENT_KEY"] ; then
   if [[ $INSTANA_AGENT_KEY == "aws:kms:"* ]]; then
   	ACCOUNTKEY=${INSTANA_AGENT_KEY##aws:kms:}
   	ACCOUNTKEY=`python3 /opt/taupage/bin/decrypt-kms.py $ACCOUNTKEY`
@@ -22,24 +25,24 @@ else
 fi
 
 #Set instana zone for application -- e.g. AWS account alias
-if [! -z "${config_instana_zone}"] ; then
-  export INSTANA_ZONE=${config_instana_zone}
+if [! -z "$INSTANA_ZONE"] ; then
+  export INSTANA_ZONE=$INSTANA_ZONE
 else
   echo "INFO: Instana zone configuration is missing. Skipping Instana setup."
   exit 1
 
 #Set instana tags -- If not specified use the stack name from senza
-if [! -z "$config_instana_tags"]; then
+if [! -z "$INSTANA_TAGS"]; then
   export INSTANA_TAGS="$config_instana_tags,stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version"
 else
   export INSTANA_TAGS="stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version"
 
 # GET the INFRA/APM mode from environment variable
-if [! -z "${config_instana_agent_mode}"]; then
+if [! -z "$AGENTMODE"]; then
     shopt -s nocasematch
-  if [[${config_instana_agent_mode} =~ "APM"]]; then
+  if [[$AGENTMODE =~ "APM"]]; then
     AGENTMODE="APM"
-  elif [[${config_instana_agent_mode} =~ "OFF" ]]; then
+  elif [[$AGENTMODE =~ "OFF" ]]; then
     AGENTMODE="OFF"
   else
     echo "INFO: Setting instana agent mode to INFRASTRUCTURE."
