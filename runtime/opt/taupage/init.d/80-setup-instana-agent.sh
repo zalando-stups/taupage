c504cb1ba212dd5379f9a904f3c97f9ee29063d0
rajatparida86 <rajatparida86@gmail.com>
2017-09-27 11:36:54 +0200

    added support for instana agent in taupage (#453)
    * added support for instana agent in taupage
    
    * Added default tags
    
    * add instana agent install process
    
    * Better data validation and updated instana auto update mode
    
    * Inserted instead of Updated in the agent.cfg file
    
    * Corrected setting instana key env variable
    
    * add readable variables.
    
    * fix sed command
    
    * modify apt-get install command to be quiet and install everything.

---
+++ runtime/opt/taupage/init.d/80-setup-instana-agent.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# read global taupage config
+eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
+INSTANA_AGENT_KEY=${config_instana_agent_key}
+INSTANA_ZONE=${config_instana_zone}
+INSTANA_TAGS=$config_instana_tags
+AGENTMODE=${config_instana_agent_mode}
+
+#Set instana environment variables#
+#export INSTANA_AGENT_HOST=$config_instana_agent_host
+#export INSTANA_AGENT_PORT=$config_instana_agent_port
+# Set INSTANA_AGENT_KEY as ENV variable. If KMS encrypted, decrypt KMS and save to INSTANA_AGENT_KEY variable
+if [! -z "$INSTANA_AGENT_KEY"] ; then
+  if [[ $INSTANA_AGENT_KEY == "aws:kms:"* ]]; then
+  	ACCOUNTKEY=${INSTANA_AGENT_KEY##aws:kms:}
+  	ACCOUNTKEY=`python3 /opt/taupage/bin/decrypt-kms.py $ACCOUNTKEY`
+  	#overwrite INSTANA_AGENT_KEY with decrypted value
+  	INSTANA_AGENT_KEY=$ACCOUNTKEY
+  fi
+  export INSTANA_AGENT_KEY=$INSTANA_AGENT_KEY
+else
+  echo "INFO: Instana access key is missing. Skipping Instana setup."
+  exit 1
+fi
+
+#Set instana zone for application -- e.g. AWS account alias
+if [! -z "$INSTANA_ZONE"] ; then
+  export INSTANA_ZONE=$INSTANA_ZONE
+else
+  echo "INFO: Instana zone configuration is missing. Skipping Instana setup."
+  exit 1
+
+#Set instana tags -- If not specified use the stack name from senza
+if [! -z "$INSTANA_TAGS"]; then
+  export INSTANA_TAGS="$config_instana_tags,stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version"
+else
+  export INSTANA_TAGS="stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version"
+
+# GET the INFRA/APM mode from environment variable
+if [! -z "$AGENTMODE"]; then
+    shopt -s nocasematch
+  if [[$AGENTMODE =~ "APM"]]; then
+    AGENTMODE="APM"
+  elif [[$AGENTMODE =~ "OFF" ]]; then
+    AGENTMODE="OFF"
+  else
+    echo "INFO: Setting instana agent mode to INFRASTRUCTURE."
+    AGENTMODE="INFRASTRUCTURE"
+  fi
+else
+  AGENTMODE="INFRASTRUCTURE"
+  echo "WARN: Instana agent mode not specified in Senza. Falling back to Instana infrastructure monitoring."
+fi
+# Set the INFRA/APM mode in file -- instana-agent/etc/instana/com.instana.agent.main.config.Agent.cfg
+agentConfig = /opt/instana/agent/etc/instana/com.instana.agent.main.config.Agent.cfg
+echo "mode = $AGENTMODE" >> $agentConfig
