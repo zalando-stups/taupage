8f9ee28c6892e041e5bb29353b239ff5842ba614
Rajat Parida <rajat.parida@zalando.de>
2017-10-13 11:54:42 +0200

    Added the feature to insert custom tags into /opt/instana/agent/etc/instana/configuration.yaml

--- runtime/opt/taupage/init.d/80-setup-instana-agent.sh
+++ runtime/opt/taupage/init.d/80-setup-instana-agent.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-
 # read global taupage config
 eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
 INSTANA_AGENT_KEY=${config_instana_agent_key}
 INSTANA_ZONE=${config_instana_zone}
-INSTANA_TAGS=$config_instana_tags
+INSTANA_TAGS=${config_instana_tags}
 AGENTMODE=${config_instana_agent_mode}
 
 #Set instana environment variables#
 #export INSTANA_AGENT_HOST=$config_instana_agent_host
 #export INSTANA_AGENT_PORT=$config_instana_agent_port
 # Set INSTANA_AGENT_KEY as ENV variable. If KMS encrypted, decrypt KMS and save to INSTANA_AGENT_KEY variable
-if [ "$INSTANA_AGENT_KEY" ] ; then
-  if [[ $INSTANA_AGENT_KEY == "aws:kms:"* ]]; then
-  	ACCOUNTKEY=${INSTANA_AGENT_KEY##aws:kms:}
-  	ACCOUNTKEY=`python3 /opt/taupage/bin/decrypt-kms.py $ACCOUNTKEY`
-  	#overwrite INSTANA_AGENT_KEY with decrypted value
-  	INSTANA_AGENT_KEY=$ACCOUNTKEY
-  fi
-  export INSTANA_AGENT_KEY=$INSTANA_AGENT_KEY
-  echo "INSTANA_AGENT_KEY=$INSTANA_AGENT_KEY" >> /etc/environment
-else
-  echo "INFO: Instana access key is missing. Skipping Instana setup."
-  exit 0
-fi
+# if [ "$INSTANA_AGENT_KEY" ] ; then
+#   if [[ $INSTANA_AGENT_KEY == "aws:kms:"* ]]; then
+#   	ACCOUNTKEY=${INSTANA_AGENT_KEY##aws:kms:}
+#   	ACCOUNTKEY=`python3 /opt/taupage/bin/decrypt-kms.py $ACCOUNTKEY`
+#   	#overwrite INSTANA_AGENT_KEY with decrypted value
+#   	INSTANA_AGENT_KEY=$ACCOUNTKEY
+#   fi
+#   export INSTANA_AGENT_KEY=$INSTANA_AGENT_KEY
+#   echo "INSTANA_AGENT_KEY=$INSTANA_AGENT_KEY" >> /etc/environment
+# else
+#   echo "INFO: Instana access key is missing. Skipping Instana setup."
+#   exit 0
+# fi
 
-#Set instana zone for application -- e.g. AWS account alias
+#Set instana zone for application -- e.g. AWS account alias in configurationYaml=/opt/instana/agent/etc/instana/configuration.yaml
 configurationYaml=/opt/instana/agent/etc/instana/configuration.yaml
 if [ "$INSTANA_ZONE" ] ; then
   export INSTANA_ZONE=$INSTANA_ZONE
@@ -38,14 +37,34 @@ else
   exit 0
 fi
 
-#Set instana tags -- If not specified use the stack name from senza
+#Set instana tags in /opt/instana/agent/etc/instana/configuration.yaml
 if [ "$INSTANA_TAGS" ]; then
   export INSTANA_TAGS="$config_instana_tags,stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version"
   echo "INSTANA_TAGS=$config_instana_tags,stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version" >> /etc/environment
+
+  tags="tags:"
+  customTags=$config_instana_tags
+  splitTags=$(echo $customTags | tr "," "\n")
+  for tag in $splitTags
+    do
+    tags="$tags\\
+     - '$tag'"
+    done
+  tags="\\
+     - 'stack_name=$config_notify_cfn_stack'\\
+     - 'application_id=$config_application_id'\\
+     - 'aplication_version=$config_application_version'"
 else
+  tags="tags:\\
+     - 'stack_name=$config_notify_cfn_stack'\\
+     - 'application_id=$config_application_id'\\
+     - 'aplication_version=$config_application_version'"
+
   export INSTANA_TAGS="stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version"
   echo "INSTANA_TAGS=stack_name=$config_notify_cfn_stack,application_id=$config_application_id,aplication_version=$config_application_version" >> /etc/environment
 fi
+sed -i -e "1, $ s/#com.instana.plugin.host.*/com.instana.plugin.host:/" $configurationYaml
+sed -i -e "1, $ s/#  tags:.*/  $tags/" $configurationYaml
 
 # GET the INFRA/APM mode from environment variable
 if [ "$AGENTMODE" ]; then
