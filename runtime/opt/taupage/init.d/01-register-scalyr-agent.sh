c54ab92ac9a50d0a3aaa15621eedd1861e1bae37
Felix Mueller <felix.mueller@zalando.de>
2017-10-13 14:35:07 +0200

    Allow setting Scalyr region to "eu" (#465)
    * Allow adding a custom scalyr host
    
    This is a preparation to allow setting custom Scalyr hosts.
    
    * Simplify host/region configuration
    
    * s/host/region
    
    * Catch eu + EU and more verbose output

--- runtime/opt/taupage/init.d/01-register-scalyr-agent.sh
+++ runtime/opt/taupage/init.d/01-register-scalyr-agent.sh
@@ -9,6 +9,7 @@ eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
 ACCOUNTKEY=$config_scalyr_account_key
 APPID=$config_application_id
 APPVERSION=$config_application_version
+SCALYR_REGION=$config_scalyr_region
 SOURCE=$config_source
 STACK=$config_notify_cfn_stack
 IMAGE=$(echo "$SOURCE" | awk -F \: '{ print $1 }')
@@ -44,6 +45,24 @@ then
     fi
 else
     echo "INFO: scalyr not configured; skipping daemon setup.";
+    exit;
+fi
+
+# Allow custom Scalyr region
+if [[ "$SCALYR_REGION" == "eu" ]] || [[ "$SCALYR_REGION" == "EU" ]];
+then
+
+    echo -n "Configuring Scalyr region to eu.scalyr.com ... ";
+    /usr/sbin/scalyr-agent-2-config --set-server-host "https://upload.eu.scalyr.com"
+    if [ $? -eq 0 ];
+    then
+        echo -n "DONE"
+    else
+        echo -n "ERROR: Setting custom Scalyr region failed";
+        exit;
+    fi
+else
+    echo "INFO: Scalyr region config not set; skipping region setup.";
     exit;
 fi
 
