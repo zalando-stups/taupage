fdaf732aba1e192b9c5e8aafa03ac60d004a1fa3
aermakov-zalando <alexey.ermakov@zalando.de>
2017-10-20 13:23:45 +0200

    Improve logrotate use (#462)
    * logrotate: run every 1 minute
    
     - Run logrotate every 1 minute instead of hourly + daily
     - Split statefile cleanup into a separate job running hourly
     - Use flock(1) to avoid running logrotate or cleanup concurrently
    
    * Remove useless build/files entry
    
    * Make calling logrotate more often configurable
    
     * default to the previous behavior (once every hour)
     * add a logrotate_interval_minutes setting to allow running logrotate
       more frequently
    
    * _logrotate: fix cron's broken environment
    
    * Replace the cron.d file atomically
    
    * Add support for rsyslog_application_hardlimit
    
    Implement a configuration option for a hard limit on application.log
    size. If it's set, rsyslogd will be reconfigured with a size limit on
    /var/log/application.log and will forcibly invoke logrotate if the size
    limit is breached. Disabled by default for now, let's see how it
    behaves.
    
    * Fix flake8 issues

---
+++ runtime/opt/taupage/init.d/04-configure-logrotate.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import taupage
+
+CRON_TEMPLATE = "*/{interval} * * * *   root    /usr/local/sbin/_logrotate"
+
+
+def logrotate_interval(config):
+    result = config.get('logrotate_interval_minutes')
+    if result is not None:
+        try:
+            result = int(result)
+            if result < 1 or result >= 60:
+                raise ValueError("logrotate_interval_minutes must be between 1 and 59")
+            return result
+        except ValueError as e:
+            logging.error("Invalid configuration for logrotate_interval_minutes")
+            logging.exception(e)
+
+
+def main():
+    taupage.configure_logging()
+    config = taupage.get_config()
+
+    interval = logrotate_interval(config)
+    if interval:
+        logging.info("Reconfiguring logrotate to run every {} minutes".format(interval))
+        with open("/etc/.logrotate.tmp", "w") as f:
+            f.write(CRON_TEMPLATE.format(interval=interval))
+            f.write("\n")
+        os.rename("/etc/.logrotate.tmp", "/etc/cron.d/logrotate")
+
+
+if __name__ == '__main__':
+    main()
