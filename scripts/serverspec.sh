d19fd8845d0b9337fdc8b7c43fa071d82441593b
Marco Hoyer <marco.hoyer@scout24.com>
2017-12-13 13:49:26 +0100

    install rake from apt repo to have a compatible version for the installed ruby, latest version requires ruby 2 which I do not want to update

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Preparing Serverspec and running system tests..."
 # sudo apt-get update &&
-apt-get install -y ruby1.9.1
+apt-get install -y ruby1.9.1 rake
 gem install net-ssh -v 2.9.2 --no-ri --no-rdoc
  
-gem install bundler rake serverspec --no-ri --no-rdoc
+gem install bundler serverspec --no-ri --no-rdoc
 chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
 rake
