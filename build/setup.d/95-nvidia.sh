89e91409e23d6b3fd3c56bf0c3ccf40d76fb5012
Alexey Ermakov <alexey.ermakov@zalando.de>
2018-01-25 11:30:29 +0100

    Switch to sha256 instead of md5

--- build/setup.d/95-nvidia.sh
+++ build/setup.d/95-nvidia.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 DRIVER_ARCH="Linux-x86_64"
 DRIVER_VERSION="384.90"
 DRIVER_FILENAME="NVIDIA-${DRIVER_ARCH}-${DRIVER_VERSION}.run"
-DRIVER_CHECKSUM="487f9702d76d9eebea5b73b33fe4d602"
+DRIVER_CHECKSUM="0ddf6820f2fcca3ec3021e42a028f8bc08bca123fcea4c0c3f41c8c0ffa5febd"
 
 DOCKER_DRIVER_VERSION="1.0.1"
 DOCKER_DRIVER_FILENAME="nvidia-docker_${DOCKER_DRIVER_VERSION}-1_amd64.deb"
@@ -29,7 +29,7 @@ echo "Installing NVIDIA driver ver: ${DRIVER_VERSION}"
 # Using the run File
 wget -P /tmp "http://us.download.nvidia.com/XFree86/${DRIVER_ARCH}/${DRIVER_VERSION}/${DRIVER_FILENAME}"
 
-echo "$DRIVER_CHECKSUM /tmp/$DRIVER_FILENAME" | md5sum -c
+echo "$DRIVER_CHECKSUM /tmp/$DRIVER_FILENAME" | sha256sum -c
 
 chmod +x "/tmp/${DRIVER_FILENAME}"
 
