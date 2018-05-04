c2508c76cf6e550b8ae555840adae67ff9de44f9
Alexey Ermakov <alexey.ermakov@zalando.de>
2018-05-04 12:03:55 +0200

    syslog: optionally add AWS metadata to the logs
    If rsyslog_aws_metadata is set in Taupage config, add AWS account ID and
    region to the log lines. This is useful for Scalyr S3 archival, because
    it only exports the raw log lines which means it's impossible to
    determine which account the message came from.

--- runtime/opt/taupage/init.d/04-configure-rsyslogd.sh
+++ runtime/opt/taupage/init.d/04-configure-rsyslogd.sh
@@ -3,10 +3,37 @@
 eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
 
 rsyslog_max_message_size=$config_rsyslog_max_message_size
+rsyslog_aws_metadata=$config_rsyslog_aws_metadata
 rsyslog_config=/etc/rsyslog.conf
 
 if [[ -n "$rsyslog_max_message_size" ]]; then
     echo "setting rsyslog_max_message_size..."
     grep -q '^$MaxMessageSize' $rsyslog_config || sed -i "1s/^/\$MaxMessageSize ${rsyslog_max_message_size}\n/" $rsyslog_config
+fi
+
+if [[ -n "$rsyslog_aws_metadata" ]]; then
+    echo "enabling AWS metadata in rsyslog..."
+    IID="$(curl --fail -s http://169.254.169.254/latest/dynamic/instance-identity/document)"
+    if [ $? -eq 0 ]; then
+      AWS_ACCOUNT="$(echo "$IID" | jq -r .accountId)"
+      AWS_REGION="$(echo "$IID" | jq -r .region)"
+      cat >/etc/rsyslog.d/00-template.conf <<EOF
+template(name="DefaultFormat" type="list") {
+    property(name="timestamp" dateFormat="rfc3164")
+    constant(value=" $AWS_ACCOUNT $AWS_REGION ")
+    property(name="hostname")
+    constant(value=" ")
+    property(name="syslogtag")
+    property(name="msg" spifno1stsp="on" )
+    property(name="msg" droplastlf="on" )
+    constant(value="\n")
+}
+
+\$ActionFileDefaultTemplate DefaultFormat
+EOF
+    fi
+fi
+
+if [[ -n "$rsyslog_max_message_size" ]] || [[ -n "$rsyslog_aws_metadata" ]]; then
     service rsyslog restart
 fi
