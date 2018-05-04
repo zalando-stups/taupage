4ebcf77ec29aca9e3d7ddb1db39e9324de0b76b6
Alex Beverage <abeverage@zalando-26362.corp.ad.zalando.net>
2018-05-04 15:23:58 +0200

    Adding compression

--- runtime/opt/taupage/init.d/01-register-scalyr-agent.sh
+++ runtime/opt/taupage/init.d/01-register-scalyr-agent.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # read /meta/taupage.yaml
 # get scalyr Key and register the agent
-
 #read taupage.yaml file
 eval $(/opt/taupage/bin/parse-yaml.py /meta/taupage.yaml "config")
-
 #set more readable variables
 ACCOUNTKEY=$config_scalyr_account_key
 APPID=$config_application_id
@@ -16,24 +14,20 @@ IMAGE=$(echo "$SOURCE" | awk -F \: '{ print $1 }')
 LOGPARSER=${config_scalyr_application_log_parser:-slf4j}
 CUSTOMLOG=$config_mount_custom_log
 CUSTOMPARSER=${config_scalyr_custom_log_parser:-slf4j}
-
 #check if appname and appversion is provided from the yaml
 if [ -z "$APPID" ] && [ -z "$APPVERSION" ];
 then
     echo "ERROR: no application_id and application_version are in the yaml files";
     exit;
 fi
-
 # If KMS encrypted, decrypt KMS and save to ACCOUNTKEY variable
 if [[ $ACCOUNTKEY == "aws:kms:"* ]]; then
   ACCOUNTKEY=${ACCOUNTKEY##aws:kms:}
   ACCOUNTKEY=`python3 /opt/taupage/bin/decrypt-kms.py $ACCOUNTKEY`
 fi
-
 #If Scalyr account exists in the yaml file. Register the Scalyr Daemon to this Account
 if [ -n "$ACCOUNTKEY" ];
 then
-
     echo -n "Configuring scalyr daemon ... ";
     /usr/sbin/scalyr-agent-2-config --set-key "$ACCOUNTKEY"
     if [ $? -eq 0 ];
@@ -47,14 +41,11 @@ else
     echo "INFO: scalyr not configured; skipping daemon setup.";
     exit;
 fi
-
 #default path to scalyr config
 scalyr_config=/etc/scalyr-agent-2/agent.json
-
 #set serverhost to application_id
 echo -n "set app name and version ...";
 sed -i "/\/\/ serverHost: \"REPLACE THIS\"/s@.*@  serverHost:\ \"$APPID\", application_id: \"$APPID\", application_version: \"$APPVERSION\", stack: \"$STACK\", source: \"$SOURCE\", image:\"$IMAGE\"@" $scalyr_config
-
 if [ $? -eq 0 ];
 then
     echo -n "DONE";
@@ -62,11 +53,11 @@ else
     echo -n "ERROR";
     exit
 fi
-
 #disable system metric
 echo "";
 echo -n "disable system metrics ... ";
-sed -i "/api_key\:/a\ \ implicit_metric_monitor: false, implicit_agent_process_metrics_monitor: false, " $scalyr_config
+sed -i "/api_key\:/a\ \ implicit_metric_monitor: false," $scalyr_config
+sed -i "/api_key\:/a\ \ implicit_agent_process_metrics_monitor: false, " $scalyr_config
 if [ $? -eq 0 ];
 then
     echo -n "DONE";
@@ -74,8 +65,6 @@ else
     echo -n "ERROR";
     exit
 fi
-
-
 #follow syslog
 echo "";
 echo -n "insert syslog to follow ... ";
@@ -87,8 +76,6 @@ else
     echo -n "ERROR";
     exit
 fi
-
-
 #follow auth.log
 echo "";
 echo -n "insert authlog to follow ... ";
@@ -101,7 +88,6 @@ else
     echo -n "ERROR";
     exit
 fi
-
 #follow application.log
 echo "";
 echo -n "insert application to follow ... ";
@@ -114,7 +100,6 @@ else
     echo -n "ERROR";
     exit
 fi
-
 #follow custom logs if it's enabled in senza.yaml
 if [ -n "$CUSTOMLOG" ];
 then
@@ -130,7 +115,6 @@ then
       exit
   fi
 fi
-
 #add max_log_offset_size
 echo "";
 echo -n "adding max_log_offset_size... ";
@@ -143,7 +127,6 @@ else
     echo -n "ERROR";
     exit
 fi
-
 #add max_log_offset_size
 echo "";
 echo -n "setting debug_init to true... ";
@@ -156,11 +139,21 @@ else
     echo -n "ERROR";
     exit
 fi
-
+#add compressionType
+echo "";
+echo -n "setting debug_init to true... ";
+sed -i '/api_key/a \  compressionType: "bz2",' $scalyr_config
+if [ $? -eq 0 ];
+then
+    echo -n "DONE";
+    echo "";
+else
+    echo -n "ERROR";
+    exit
+fi
 # Allow custom Scalyr region
 if [[ "$SCALYR_REGION" == "eu" ]] || [[ "$SCALYR_REGION" == "EU" ]];
 then
-
     echo -n "Configuring Scalyr region to eu.scalyr.com ... ";
     sed -i '/api_key/a \  \scalyr_server: "https://upload.eu.scalyr.com",' $scalyr_config
     if [ $? -eq 0 ];
@@ -174,7 +167,6 @@ else
     echo "INFO: Scalyr region config not set; skipping region setup.";
     exit;
 fi
-
 echo -n "restarting scalyr daemon ... ";
 /usr/sbin/scalyr-agent-2 stop # just in case
 /usr/sbin/scalyr-agent-2 start
