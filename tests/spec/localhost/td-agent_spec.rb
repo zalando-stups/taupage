2c49532e45c2b7dca13e6bd7dd80564c48ae9b3b
Felix Mueller <felix.mueller@zalando.de>
2018-06-26 15:29:45 +0200

    Add tests for td-agent

---
+++ tests/spec/localhost/td-agent_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Test if td-agent port is listening
+describe port(8888) do
+  it { should be_listening.on('127.0.0.1').with('tcp') }
+end
+
+describe package('td-agent') do
+  it { should be_installed }
+end
+
+describe service('td-agent') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
