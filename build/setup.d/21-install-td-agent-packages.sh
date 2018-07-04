bd96cbb74e22c40412223034d8eb2c3d74a495f8
Felix Mueller <felix.mueller@zalando.de>
2018-07-04 16:16:09 +0200

    Ensure that directories are writable and exist

--- build/setup.d/21-install-td-agent-packages.sh
+++ build/setup.d/21-install-td-agent-packages.sh
@@ -1,5 +1,9 @@
 # Ensure that /etc/init.d/td-agent is removed
 rm /etc/init.d/td-agent
 
+# Ensure that directories are writable
+chmod 1777 /tmp/
+mkdir -p -m0755 /var/run/td-agent
+
 # Install Fluentd packages
 td-agent-gem install fluent-plugin-scalyr
