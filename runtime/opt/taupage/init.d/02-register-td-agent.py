fee0266e7ef027bb3feb326aa7d8fffc4370d3e4
Felix Mueller <felix.mueller@zalando.de>
2018-07-11 16:46:12 +0200

    deactivate script

--- runtime/opt/taupage/init.d/02-register-td-agent.py
+++ runtime/opt/taupage/init.d/02-register-td-agent.py
@@ -2,23 +2,23 @@
 
 # import logging
 # import os
-import subprocess
+# import subprocess
 
 # from taupage import get_config
 
-TD_AGENT_TEMPLATE = ''
+# TD_AGENT_TEMPLATE = ''
 
 
-def start_td_agent_process():
-    process = subprocess.Popen(['service', 'td-agent', 'start'])
-    exit_code = process.wait()
-    if exit_code:
-        raise Exception("'service td-agent start'failed with exit code: {0}".format(exit_code))
+# def start_td_agent_process():
+#    process = subprocess.Popen(['service', 'td-agent', 'start'])
+#    exit_code = process.wait()
+#    if exit_code:
+#        raise Exception("'service td-agent start'failed with exit code: {0}".format(exit_code))
 
 
-def set_scalyr_api_key():
-    pass
+# def set_scalyr_api_key():
+#     pass
 
 
-def configure_scalyr():
-    pass
+# def configure_scalyr():
+#     pass
