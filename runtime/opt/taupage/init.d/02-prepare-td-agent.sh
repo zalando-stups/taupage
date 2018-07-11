f044488486661377da59f3c9340b0b20e1c57da9
Felix Mueller <felix.mueller@zalando.de>
2018-07-11 11:23:47 +0200

    Modified upstart changes

--- runtime/opt/taupage/init.d/02-prepare-td-agent.sh
+++ runtime/opt/taupage/init.d/02-prepare-td-agent.sh
@@ -4,7 +4,4 @@
 chmod -R 1777 /tmp/
 mkdir -p -m0755 /var/run/td-agent
 
-# Ensure that td-agent can read logs from /var/log/application.log
-chmod 644 /var/log/application.log
-
 service td-agent start
