354d2c47a9fe56abf92c7b3c9d5afa819d5efd4c
Felix Mueller <felix.mueller@zalando.de>
2018-07-11 11:54:27 +0200

    Adding runtime/opt/taupage/init.d/02-register-td-agent.py skeleton + template file for configuration

---
+++ runtime/opt/taupage/init.d/02-register-td-agent.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import subprocess
+
+from taupage import get_config
+
+TD_AGENT_TEMPLATE = ''
+
+
+def start_td_agent_process():
+    process = subprocess.Popen(['service', 'td-agent', 'start'])
+    exit_code = process.wait()
+    if exit_code:
+        raise Exception("'service td-agent start'failed with exit code: {0}".format(exit_code))
+
+
+def set_scalyr_api_key():
+    pass
+
+
+def configure_scalyr():
+    pass
+
