4b690188f008c674fa2cc64b0e7f006678947a06
pluralistix <christian.weigmann@scout24.com>
2018-08-03 13:33:32 +0200

    ping net-telnet to version 0.1.1

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -3,7 +3,8 @@ echo "Preparing Serverspec and running system tests..."
 apt-get update &&
 apt-get install -y ruby1.9.1 rake
 gem install net-ssh -v 2.9.2 --no-ri --no-rdoc
- 
+
+gem install net-telnet -v 0.1.1 --no-ri --no-rdoc 
 gem install bundler serverspec --no-ri --no-rdoc
 chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
