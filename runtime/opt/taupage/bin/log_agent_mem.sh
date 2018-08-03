792ee334e5df511f5abdab0dd76623d7458fca48
Veit Wiessner <veit.wiessner@zalando.net>
2018-08-03 13:00:44 +0200

    added cpu usage

--- runtime/opt/taupage/bin/log_agent_mem.sh
+++ runtime/opt/taupage/bin/log_agent_mem.sh
@@ -4,9 +4,13 @@ TDAGENT_PID=$(ps aux | grep "td-agent" | grep -v grep | awk '{print $2}')
 SCALYR_PID=$(ps aux | grep "scalyr" | grep -v grep | awk '{print $2}')
 
 if [ ! -z "${TDAGENT_PID}" ];then
-   logger "td-agent memory usage: $(pmap -X ${TDAGENT_PID} | tail -1 | awk '{print $3}')"
+   PSS=$(pmap -X ${TDAGENT_PID} | tail -1 | awk '{print $3}')
+   CPU=$(top -b -n 1 -p ${TDAGENT_PID} | tail -1 | awk '{print $9}')
+   logger "td-agent cpu and memory usage: ${CPU} ${PSS}"
 fi
 
 if [ ! -z "${SCALYR_PID}" ];then
-   logger "scalyr-agent-2 memory usage: $(pmap -X ${SCALYR_PID} | tail -1 | awk '{print $3}')"
+   PSS=$(pmap -X ${SCALYR_PID} | tail -1 | awk '{print $3}')
+   CPU=$(top -b -n 1 -p ${SCALYR_PID} | tail -1 | awk '{print $9}')
+   logger "scalyr-agent-2 cpu and memory usage: ${CPU} ${PSS}"
 fi
