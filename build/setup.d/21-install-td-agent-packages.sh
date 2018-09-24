de067645a123902faf1e44be79ab6cc9bed4a85c
Felix Mueller <felix.mueller@zalando.de>
2018-09-24 10:33:25 +0200

    Add optional Fluentd support

---
+++ build/setup.d/21-install-td-agent-packages.sh
@@ -0,0 +1,14 @@
+# Ensure that /etc/init.d/td-agent is removed
+rm /etc/init.d/td-agent
+
+# Ensure that directories are writable
+chmod -R 1777 /tmp/
+mkdir -p -m0755 /var/run/td-agent
+
+# Get SSL certificate chain for Scalyr plugin
+wget https://curl.haxx.se/ca/cacert.pem -O /etc/ssl/certs/scalyr-ca-bundle.crt
+
+# Install Fluentd packages
+td-agent-gem install fluent-plugin-scalyr \
+					 fluent-plugin-prometheus fluent-plugin-s3 \
+					 fluent-plugin-remote_syslog
