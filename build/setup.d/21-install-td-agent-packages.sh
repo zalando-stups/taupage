d241ae6ea36249180b498f24b6a1581cba47f9f0
Felix Mueller <felix.mueller@zalando.de>
2018-09-24 10:45:49 +0200

    s/packages/plugins
    Signed-off-by: Felix Mueller <felix.mueller@zalando.de>

--- build/setup.d/21-install-td-agent-packages.sh
+++ build/setup.d/21-install-td-agent-packages.sh
@@ -8,7 +8,7 @@ mkdir -p -m0755 /var/run/td-agent
 # Get SSL certificate chain for Scalyr plugin
 wget https://curl.haxx.se/ca/cacert.pem -O /etc/ssl/certs/scalyr-ca-bundle.crt
 
-# Install Fluentd packages
+# Install Fluentd plugins
 td-agent-gem install fluent-plugin-scalyr \
 					 fluent-plugin-prometheus fluent-plugin-s3 \
 					 fluent-plugin-remote_syslog
