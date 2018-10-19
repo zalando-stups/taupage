eecd50c5e2f68b5d052b7bc7d7394eec1fd3783d
Felix Mueller <felix.mueller@zalando.de>
2018-10-19 17:35:52 +0200

    Updating Ruby version for tests

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -3,9 +3,9 @@ echo "Preparing Serverspec and running system tests..."
 # sudo apt-get update &&
 # apt-get install -y ruby1.9.1
 # Install ruby
-add-apt-repository ppa:brightbox/ruby-ng-experimental
+add-apt-repository ppa:brightbox/ruby-ng
 apt-get update
-apt-get install -y ruby2.1 ruby2.1-dev gcc
+apt-get install -y ruby2.4 ruby2.4-dev gcc
 gem install bundler rake serverspec --no-ri --no-rdoc
 chmod +x /tmp/scripts/serverspec.sh
 cd /tmp/tests
