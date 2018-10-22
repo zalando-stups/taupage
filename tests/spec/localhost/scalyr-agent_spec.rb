d0834f431a991a10c00febed735c6a8295fdd6a5
Felix Mueller <felix.mueller@zalando.de>
2018-10-22 16:10:25 +0200

    Changing test string

--- tests/spec/localhost/scalyr-agent_spec.rb
+++ tests/spec/localhost/scalyr-agent_spec.rb
@@ -7,8 +7,8 @@ config = YAML.load_file('/meta/taupage.yaml')
 # Read taupage.yaml to find out if scalyr is configured and check if agent.json contains expected file paths
 if config['scalyr_account_key']
   describe file('/etc/scalyr-agent-2/agent.json') do
-    it { should contain 'application.log' }
-    it { should contain 'auth.log' }
-    it { should contain 'syslog' }
+    it { should contain '/var/log/application.log' }
+    it { should contain '/var/log/auth.log' }
+    it { should contain '/var/log/syslog' }
   end
 end
