0c5f811564aaf27b58ede9d3041bd26442bfa57c
Veit Wiessner <veit.wiessner@zalando.de>
2019-03-12 17:16:49 +0100

    purged check for /var/log/application.log permissions
    Signed-off-by: Veit Wiessner <veit.wiessner@zalando.de>

--- tests/spec/localhost/td-agent_spec.rb
+++ tests/spec/localhost/td-agent_spec.rb
@@ -19,11 +19,6 @@ describe file('/etc/init.d/td-agent') do
   it { should_not exist }
 end
 
-# Ensure that /var/log/application.log is readable (chmod 644
-#describe file('/var/log/application.log') do
-#  it { should be_mode 644 }
-#end
-
 # Check if Scalyr output plugin is installed
 describe command('td-agent-gem list') do
   its(:stdout) { should contain('fluent-plugin-scalyr') }
