c2f49a2da772bcaf5a891046590cd74dc096792c
Arjun <arjunrn@users.noreply.github.com>
2019-04-02 13:49:28 +0200

    Reverting the logging to S3 changes (#547)
    * Reverting the logging to S3 changes
    
    Signed-off-by: Arjun Naik <arjun.rn@gmail.com>
    
    * Added sanity checks back
    
    Signed-off-by: Arjun Naik <arjun.rn@gmail.com>

--- build/setup.d/01-add-node_exporter-text_collector-dir.sh
+++ build/setup.d/01-add-node_exporter-text_collector-dir.sh
@@ -2,5 +2,4 @@
 
 mkdir -p -m0755 /var/local/textfile_collector
 
-echo "fluentd_process_running{tag=\"td-agent\",hostname=\"$(hostname)\"} 0.0" > /var/local/textfile_collector/fluentd_metrics.prom
-echo "fluentd_default_s3_logging{tag=\"td-agent\",hostname=\"$(hostname)\"} 0.0" > /var/local/textfile_collector/fluentd_default_s3.prom
+echo "fluentd{tag=\"fluentd_enabled\", hostname=\"${hostname}\"} 0.0" > /var/local/textfile_collector/fluentd_metrics.prom
