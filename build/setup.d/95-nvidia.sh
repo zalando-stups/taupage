2e58067bf6a84c7a4fac825ece6c7661c1382429
Arjun Naik <arjun.rn@gmail.com>
2019-04-11 14:12:13 +0200

    Install GPU drivers from PPA and added hello world test
    Signed-off-by: Arjun Naik <arjun.rn@gmail.com>

--- build/setup.d/95-nvidia.sh
+++ build/setup.d/95-nvidia.sh
@@ -1,43 +1,13 @@
 #!/bin/bash
 
-# See https://github.com/NVIDIA/nvidia-docker/wiki/Deploy-on-Amazon-EC2
-# But we're using the run script to not depend on build tools
 set -euo pipefail
 
-DRIVER_ARCH="Linux-x86_64"
-DRIVER_VERSION="410.104"
-DRIVER_FILENAME="NVIDIA-${DRIVER_ARCH}-${DRIVER_VERSION}.run"
-DRIVER_CHECKSUM="6ac4f7355a11e4b6ea2ff5b3d6ea677247f286daecf2e9373ef947ee08682cb7"
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 
-DOCKER_DRIVER_VERSION="1.0.1"
-DOCKER_DRIVER_FILENAME="nvidia-docker_${DOCKER_DRIVER_VERSION}-1_amd64.deb"
-
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  tee /etc/apt/sources.list.d/nvidia-docker.list
 
 apt-get update
+apt-get install -y --no-install-recommends nvidia-docker nvidia-384 libcuda1-384 nvidia-modprobe
 
-pkgs="
-build-essential
-linux-headers-virtual-lts-xenial
-"
-
-# find latest headers
-kernelname=$(ls -lah /usr/src/ | tail -n1 | sed 's/.*linux-headers-\([0-9a-z.-]\+\).*/\1/')
-apt-get install -y -q --no-install-recommends -o Dpkg::Options::="--force-confold" $pkgs >>install.log
-
-echo "Installing NVIDIA driver ver: ${DRIVER_VERSION}"
-
-# Using the run File
-wget -P /tmp "https://download.nvidia.com/XFree86/${DRIVER_ARCH}/${DRIVER_VERSION}/${DRIVER_FILENAME}"
-
-echo "$DRIVER_CHECKSUM /tmp/$DRIVER_FILENAME" | sha256sum -c
-
-chmod +x "/tmp/${DRIVER_FILENAME}"
-
-/tmp/$DRIVER_FILENAME -e -a -s --kernel-source-path "/usr/src/linux-headers-${kernelname}/" --kernel-name "${kernelname}"
-
-echo "Installing nvidia docker ver: ${DOCKER_DRIVER_VERSION}"
-wget -P /tmp "https://github.com/NVIDIA/nvidia-docker/releases/download/v${DOCKER_DRIVER_VERSION}/${DOCKER_DRIVER_FILENAME}"
-dpkg -i "/tmp/${DOCKER_DRIVER_FILENAME}"
-
-echo "Removing build packages driver"
-apt-get purge -y -q  $pkgs >>install.log
