aa2cb01cbb159fade948fbc2495163f249773b5c
Veit Wiessner <veit.wiessner@zalando.de>
2019-04-23 17:20:33 +0200

    initial commit
    Signed-off-by: Veit Wiessner <veit.wiessner@zalando.de>

--- build/setup.d/21-install-td-agent-packages.sh
+++ build/setup.d/21-install-td-agent-packages.sh
@@ -16,3 +16,21 @@ td-agent-gem install fluent-plugin-scalyr:0.8.4 \
 					 fluent-plugin-prometheus:1.1.0 \
 					 fluent-plugin-s3:1.1.6 \
 					 fluent-plugin-remote_syslog:1.0.0
+
+# limit upstart respawn to 3 tries
+
+CONF="/etc/init/td-agent.conf"
+
+rm ${CONF}
+
+echo "description     \"TD Agent\"" >> ${CONF}
+echo "stop on shutdown" >> ${CONF}
+echo "respawn limit 3 5" >> ${CONF}
+echo "pre-start script" >> ${CONF}
+echo "    mkdir -p -m0755 /var/run/td-agent" >> ${CONF}
+echo "end script" >> ${CONF}
+echo "exec /opt/td-agent/embedded/bin/ruby \
+      -Eascii-8bit:ascii-8bit /usr/sbin/td-agent \
+	  --log /var/log/td-agent/td-agent.log \
+	  --daemon /var/run/td-agent/td-agent.pid \
+	  --no-supervisor" >> ${CONF}
