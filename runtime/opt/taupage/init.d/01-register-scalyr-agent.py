7f0cd2bc72a17619fd9b94937b59d82f0a971e1c
Alexander Starnell <astarnell@users.noreply.github.com>
2019-09-12 12:23:25 +0200

    Address comments from PR Signed-off-by: Alexander Starnell <alexander.starnell@zalando.de>

--- runtime/opt/taupage/init.d/01-register-scalyr-agent.py
+++ runtime/opt/taupage/init.d/01-register-scalyr-agent.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-
+import base64
+import json
 import logging
 import subprocess
-import re
+import sys
+
 import boto.utils
-import json
-import ast
-
-
+import boto3
+import requests
+import yaml
 from taupage import get_config
+
+FAKE_CI_ACCOUNT_KEY = "foo1234"
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,7 +19,7 @@ logger = logging.getLogger(__name__)
 scalyr_agent_config_file = '/etc/scalyr-agent-2/agent.json'
 
 main_config = get_config()
-logging_config = main_config.get('logging')
+logging_config = main_config.get('logging', {})
 
 mount_custom_log = main_config.get('mount_custom_log')
 rsyslog_aws_metadata = main_config.get('rsyslog_aws_metadata')
@@ -38,78 +41,65 @@ if rsyslog_aws_metadata:
 else:
     sys_log_parser = 'systemLog'
 
-if not logging_config:
-    account_key = main_config.get('scalyr_account_key')
-    region = main_config.get('scalyr_region', 'eu')
-    application_log_parser = main_config.get('scalyr_application_log_parser', 'slf4j')
-    custom_log_parser = main_config.get('scalyr_application_log_parser', 'slf4j')
-    scalyr_agent_enabled = True
-    ship_application_log = True
-    application_log_sampling = None
-    ship_custom_log = True
-    custom_log_sampling = None
-    ship_auth_log = True
-    auth_log_sampling = None
-    ship_sys_log = True
-    sys_log_sampling = None
-else:
-    account_key = logging_config.get('scalyr_account_key')
-    region = logging_config.get('scalyr_region', 'eu')
-    application_log_parser = logging_config.get('scalyr_application_log_parser', 'slf4j')
-    custom_log_parser = logging_config.get('scalyr_application_log_parser', 'slf4j')
-    fluentd_enabled = logging_config.get('fluentd_enabled')
-    scalyr_agent_enabled = logging_config.get('scalyr_agent_enabled', not fluentd_enabled)
-    ship_all_logs = logging_config.get('use_scalyr_agent_all', not fluentd_enabled)
-    ship_application_log = logging_config.get('use_scalyr_agent_applog', ship_all_logs)
-    application_log_sampling = logging_config.get('scalyr_agent_applog_sampling')
-    ship_custom_log = logging_config.get('use_scalyr_agent_customlog', ship_all_logs)
-    custom_log_sampling = logging_config.get('scalyr_agent_customlog_sampling')
-    ship_auth_log = logging_config.get('use_scalyr_agent_authlog', ship_all_logs)
-    auth_log_sampling = logging_config.get('scalyr_agent_authlog_sampling')
-    ship_sys_log = logging_config.get('use_scalyr_agent_syslog', ship_all_logs)
-    sys_log_sampling = logging_config.get('scalyr_agent_syslog_sampling')
+account_key = logging_config.get('scalyr_account_key', main_config.get('scalyr_account_key'))
+region = logging_config.get('scalyr_region', main_config.get('scalyr_region', 'eu'))
+
+application_log_parser = logging_config.get(
+    'scalyr_application_log_parser',
+    main_config.get('scalyr_application_log_parser', 'slf4j'),
+)
+custom_log_parser = logging_config.get(
+    'scalyr_custom_log_parser',
+    main_config.get('scalyr_custom_log_parser', 'slf4j'),
+)
+
+fluentd_enabled = logging_config.get('fluentd_enabled')
+scalyr_agent_enabled = logging_config.get('scalyr_agent_enabled', not fluentd_enabled)
+ship_all_logs = logging_config.get('use_scalyr_agent_all', not fluentd_enabled)
+
+ship_application_log = logging_config.get('use_scalyr_agent_applog', ship_all_logs)
+application_log_sampling = logging_config.get('scalyr_agent_applog_sampling')
+
+ship_custom_log = logging_config.get('use_scalyr_agent_customlog', ship_all_logs)
+custom_log_sampling = logging_config.get('scalyr_agent_customlog_sampling')
+
+ship_auth_log = logging_config.get('use_scalyr_agent_authlog', ship_all_logs)
+auth_log_sampling = logging_config.get('scalyr_agent_authlog_sampling')
+
+ship_sys_log = logging_config.get('use_scalyr_agent_syslog', ship_all_logs)
+sys_log_sampling = logging_config.get('scalyr_agent_syslog_sampling')
 
 if not (scalyr_agent_enabled and account_key):
     logger.info('Found no Scalyr key or Scalyr agent not enabled; skipping Scalyr Agent init')
-    raise SystemExit()
+    sys.exit()
 
 if not (ship_application_log or ship_custom_log or ship_auth_log or ship_sys_log):
     logger.info('Nothing to do for Scalyr Agent; skipping Scalyr Agent init')
-    raise SystemExit()
+    sys.exit()
 
 
 def restart_scalyr_agent_process():
     # Hack to make the build pipeline pass
-    if account_key == "foo1234":
-        return 0
+    if account_key == FAKE_CI_ACCOUNT_KEY:
+        return
 
-    process = subprocess.Popen(['/etc/init.d/scalyr-agent-2', 'restart'])
-    exit_code = process.wait(timeout=5)
-    if exit_code:
-        raise Exception('"/etc/init.d/scalyr-agent-2 restart" failed with exit code: {0}'.format(exit_code))
+    subprocess.check_call(['/etc/init.d/scalyr-agent-2', 'restart'], timeout=5)
 
 
 def decrypt_scalyr_key():
-    scalyr_api_key = None
-    match_kms_key = re.search('aws:kms:', account_key, re.IGNORECASE)
-    if match_kms_key:
-        scalyr_api_key = re.sub(r'aws:kms:', '', account_key)
-        try:
-            scalyr_api_key = subprocess.check_output(['python3',
-                                                      '/opt/taupage/bin/decrypt-kms.py',
-                                                      scalyr_api_key]).decode('UTF-8').strip()
-        except Exception:
-            logger.error('Failed to run /opt/taupage/bin/decrypt-kms.py')
-            raise SystemExit(1)
-    if scalyr_api_key == "Invalid KMS key.":
-        logger.error('Failed to decrypt KMS Key')
-        raise SystemExit(1)
-    return scalyr_api_key
+    key_prefix = "aws:kms"
+    if not account_key.startswith(key_prefix):
+        return
+
+    region_name = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone').text[:-1]
+    client = boto3.client(service_name='kms', region_name=region_name)
+    response = client.decrypt(CiphertextBlob=base64.b64decode(account_key[len(key_prefix):]))
+    return response['Plaintext'].decode()
 
 
 def parse_string(value):
     try:
-        result = ast.literal_eval(value)
+        result = yaml.safe_load(value)
     except Exception:
         logger.warning('String \"{!s}\" could not be parsed, will be ignored!'.format(value))
         result = None
@@ -145,7 +135,7 @@ def create_config_skeleton():
     }
 
 
-def create_log_item(logfile, parser, do_jwt_redaction, sampling):
+def create_log_item(logfile, parser, sampling, do_jwt_redaction):
     item = {
         'path': logfile,
         'copy_from_start': True,
@@ -166,49 +156,37 @@ if __name__ == '__main__':
     config = create_config_skeleton()
     if ship_application_log:
         config['logs'].append(
-            create_log_item(
-                application_log_path,
-                application_log_parser,
-                True,
-                application_log_sampling
-            )
+            create_log_item(application_log_path,
+                            application_log_parser,
+                            application_log_sampling,
+                            do_jwt_redaction=True)
         )
 
     if ship_custom_log and mount_custom_log:
         config['logs'].append(
-            create_log_item(
-                custom_log_path,
-                custom_log_parser,
-                False,
-                custom_log_sampling
-            )
+            create_log_item(custom_log_path,
+                            custom_log_parser,
+                            custom_log_sampling,
+                            do_jwt_redaction=True)
         )
 
     if ship_auth_log:
         config['logs'].append(
-            create_log_item(
-                auth_log_path,
-                sys_log_parser,
-                False,
-                auth_log_sampling
-            )
+            create_log_item(auth_log_path,
+                            sys_log_parser,
+                            auth_log_sampling,
+                            do_jwt_redaction=False)
         )
 
     if ship_sys_log:
         config['logs'].append(
-            create_log_item(
-                sys_log_path,
-                sys_log_parser,
-                False,
-                sys_log_sampling
-            )
+            create_log_item(sys_log_path,
+                            sys_log_parser,
+                            sys_log_sampling,
+                            do_jwt_redaction=False)
         )
 
-    try:
-        with open(scalyr_agent_config_file, 'w') as file:
-            file.write(json.dumps(config, indent=4, sort_keys=True))
-    except Exception:
-        logger.exception('Failed to write file {!s}'.format(scalyr_agent_config_file))
-        raise SystemExit(1)
+    with open(scalyr_agent_config_file, 'w') as file:
+        json.dump(config, file, indent=4, sort_keys=True)
 
     restart_scalyr_agent_process()
