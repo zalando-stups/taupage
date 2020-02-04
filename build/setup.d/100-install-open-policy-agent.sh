c9c209a61fa5d2aa5b12eb33af9a03443ded0226
Amit Rangra <amit.rangra@zalando.de>
2020-02-04 11:49:48 +0100

    Added options while opa binary download
    Signed-off-by: Amit Rangra <amit.rangra@zalando.de>

--- build/setup.d/100-install-open-policy-agent.sh
+++ build/setup.d/100-install-open-policy-agent.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -euo pipefail
 
-curl -L -o opa https://github.com/open-policy-agent/opa/releases/download/v0.16.2/opa_linux_amd64
+curl --fail -L -o opa https://github.com/open-policy-agent/opa/releases/download/v0.16.2/opa_linux_amd64
 mv opa /opt/taupage/bin
 chmod +x /opt/taupage/bin/opa
