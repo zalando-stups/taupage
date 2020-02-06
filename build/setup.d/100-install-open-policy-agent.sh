68dc20dd526a839deddf77cafcfb230f6c77f2d4
Amit Rangra <amit.rangra@zalando.de>
2020-02-06 11:46:23 +0100

    Add sha256sum to opa binary download
    Signed-off-by: Amit Rangra <amit.rangra@zalando.de>

--- build/setup.d/100-install-open-policy-agent.sh
+++ build/setup.d/100-install-open-policy-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-curl --fail -L -o opa https://github.com/open-policy-agent/opa/releases/download/v0.16.2/opa_linux_amd64
+curl --fail -L -o opa https://github.com/open-policy-agent/opa/releases/download/v0.16.2/opa_linux_amd64 && echo "5e704decb04e8ef2963d2df209d84d3c42a73fc31c2ed61fd18b14296277411d opa" | sha256sum --check --status
 mv opa /opt/taupage/bin
 chmod +x /opt/taupage/bin/opa
